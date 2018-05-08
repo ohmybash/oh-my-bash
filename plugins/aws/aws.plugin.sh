e602c010c6bf5bce6c15a309294ae836309d91e4
Michael Anckaert <michael.anckaert@sinax.be>
2018-05-08 14:24:36 +0200

    Added first version of aws plugin

---
+++ plugins/aws/aws.plugin.sh
@@ -0,0 +1,20 @@
+# aws.plugin.sh
+# Author: Michael Anckaert <michael.anckaert@sinax.be>
+# Based on oh-my-zsh AWS plugin
+#
+# command 'agp' returns the selected AWS profile (aws get profile)
+# command 'asp' sets the AWS profile to use (aws set profile)
+#
+
+export AWS_HOME=~/.aws
+
+function agp {
+  echo $AWS_DEFAULT_PROFILE
+}
+
+function asp {
+  local rprompt=${RPROMPT/<aws:$(agp)>/}
+
+  export AWS_DEFAULT_PROFILE=$1
+  export AWS_PROFILE=$1
+}
