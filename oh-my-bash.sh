2b46e3611822676f4b021ab61e5490b4920afd87
Nataraj Basappa <n5j.b5a@gmail.com>
2018-08-09 14:13:32 +0100

    Fix for non-zero exit status

--- oh-my-bash.sh
+++ oh-my-bash.sh
@@ -134,6 +134,5 @@ if ! type_exists '__git_ps1' ; then
 fi
 
 # Adding Support for other OSes
-PREVIEW="less"
-[ -s /usr/bin/gloobus-preview ] && PREVIEW="gloobus-preview"
-[ -s /Applications/Preview.app ] && PREVIEW="/Applications/Preview.app"
+[ -s /usr/bin/gloobus-preview ] && PREVIEW="gloobus-preview" || 
+[ -s /Applications/Preview.app ] && PREVIEW="/Applications/Preview.app" || PREVIEW="less"
