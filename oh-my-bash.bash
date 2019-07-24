a99d2ef249291f9f9441d47d3dbebafc75928e1d
Jacob Hrbek <werifgx@gmail.com>
2019-07-24 03:37:46 +0200

    oh-my-bash: Sanitization for i$, not paying for dev licence

--- oh-my-bash.bash
+++ oh-my-bash.bash
@@ -95,9 +95,9 @@ done
 # Figure out the SHORT hostname
 if [[ $(uname) == Darwin* ]]; then
   # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
-  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
+  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
 else
-  SHORT_HOST=${HOST/.*/}
+  SHORT_HOST="${HOST/.*/}"
 fi
 
 # Load all of the plugins that were defined in ~/.bashrc
