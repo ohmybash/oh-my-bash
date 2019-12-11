01342b02c0f178731dd1b9aef1438064c2926310
Deepankumar <deepan0433@gmail.com>
2019-12-11 19:52:58 +0530

    brew plugin added

---
+++ plugins/brew/brew.plugin.sh
@@ -0,0 +1,8 @@
+alias brewp='brew pin'
+alias brews='brew list -1'
+alias brewsp='brew list --pinned'
+alias bubo='brew update && brew outdated'
+alias bubc='brew upgrade && brew cleanup'
+alias bubu='bubo && bubc'
+alias bcubo='brew update && brew cask outdated'
+alias bcubc='brew cask reinstall $(brew cask outdated) && brew cleanup'
