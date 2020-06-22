2d64322739160886d2a485cfb049da77da6da3f8
Deepankumar <deepan0433@gmail.com>
2020-06-22 08:18:35 +0530

    plugins: Multiple plugins implemented (#119)
    * Ansible plugin added
    
    * kubectl plugin added
    
    * NPM Plugin added
    
    * brew plugin added
    
    * shebang added

---
+++ plugins/brew/brew.plugin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+alias brewp='brew pin'
+alias brews='brew list -1'
+alias brewsp='brew list --pinned'
+alias bubo='brew update && brew outdated'
+alias bubc='brew upgrade && brew cleanup'
+alias bubu='bubo && bubc'
+alias bcubo='brew update && brew cask outdated'
+alias bcubc='brew cask reinstall $(brew cask outdated) && brew cleanup'
---
+++ plugins/npm/npm.plugin.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Install dependencies globally
+alias npmg="npm i -g "
+
+# npm package names are lowercase
+# Thus, we've used camelCase for the following aliases:
+
+# Install and save to dependencies in your package.json
+# npms is used by https://www.npmjs.com/package/npms
+alias npmS="npm i -S "
+
+# Install and save to dev-dependencies in your package.json
+# npmd is used by https://github.com/dominictarr/npmd
+alias npmD="npm i -D "
+
+# Execute command from node_modules folder based on current directory
+# i.e npmE gulp
+alias npmE='PATH="$(npm bin)":"$PATH"'
+
+# Check which npm modules are outdated
+alias npmO="npm outdated"
+
+# Check package versions
+alias npmV="npm -v"
+
+# List packages
+alias npmL="npm list"
+
+# List top-level installed packages
+alias npmL0="npm ls --depth=0"
+
+# Run npm start
+alias npmst="npm start"
+
+# Run npm test
+alias npmt="npm test"
+
+# Run npm scripts
+alias npmR="npm run"
+
+# Run npm publish 
+alias npmP="npm publish"
+
+# Run npm init
+alias npmI="npm init"
