c697d5896583eb84e1d97303882262fb4db61cc8
Seth Bergman <seth.atxwebs@gmail.com>
2021-05-31 09:42:30 -0500

    Update comments to match plugin

--- plugins/sdkman/sdkman.plugin.sh
+++ plugins/sdkman/sdkman.plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Set NVM_DIR if it isn't already defined
-[[ -z "$SDKMAN_DIR" ]] && export NVM_DIR="$HOME/.sdkman"
+# Set SDKMAN_DIR if it isn't already defined
+[[ -z "$SDKMAN_DIR" ]] && export SDKMAN_DIR="$HOME/.sdkman"
 
 # Try to load sdk only if command not already available
 if ! type "sdk" &> /dev/null; then
