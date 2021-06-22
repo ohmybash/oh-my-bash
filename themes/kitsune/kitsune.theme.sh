a96a517d560a073c0c2e357d1249527de7b74604
Altemir Pacheco <70478593+altemirpacheco@users.noreply.github.com>
2021-06-22 12:14:36 -0300

    Update kitsune.theme.sh
    Including short SHA commit to prompt

--- themes/kitsune/kitsune.theme.sh
+++ themes/kitsune/kitsune.theme.sh
@@ -28,8 +28,13 @@ function prompt_command() {
 	PS1="\n${cyan}┌─${bold_white}[\u@\h]${cyan}─${bold_yellow}(\w)$(scm_prompt_info)\n${cyan}└─${bold_green}[\A]-${green}($BC%)${bold_cyan}-[${green}${bold_green}\$${bold_cyan}]${green} "
 }
 
+function git_short_sha() {
+  SHA=$(git rev-parse --short HEAD 2> /dev/null) && echo "$SHA"
+}
+
+
 # scm theming
-SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_DIRTY="${bold_red}[$(git_short_sha)]${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 SCM_THEME_PROMPT_PREFIX="${bold_cyan}("
 SCM_THEME_PROMPT_SUFFIX="${bold_cyan})${reset_color}"
