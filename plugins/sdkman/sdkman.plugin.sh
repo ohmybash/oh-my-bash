67bd72ecd9427ebaeaf510368229b3c8774a0674
Andre Greeff <76182720+agreeff@users.noreply.github.com>
2021-06-29 10:51:37 +0200

    Fix: stop SDKMAN! plugin initialisation breaking NVM
    closes #224

--- plugins/sdkman/sdkman.plugin.sh
+++ plugins/sdkman/sdkman.plugin.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Set NVM_DIR if it isn't already defined
-[[ -z "$SDKMAN_DIR" ]] && export NVM_DIR="$HOME/.sdkman"
+# Set SDKMAN_DIR if it isn't already defined
+[[ -z "$SDKMAN_DIR" ]] && export SDKMAN_DIR="$HOME/.sdkman"
 
 # Try to load sdk only if command not already available
 if ! type "sdk" &> /dev/null; then
-  [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+  [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 fi
