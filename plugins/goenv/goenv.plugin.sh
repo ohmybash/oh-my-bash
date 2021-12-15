c076e3cfe08267eb754bc080ff594e2a0b01d1be
Wes Key <2540699+chopnico@users.noreply.github.com>
2021-12-14 20:33:58 -0500

    added a checker for .goenv directory

--- plugins/goenv/goenv.plugin.sh
+++ plugins/goenv/goenv.plugin.sh
@@ -1,12 +1,14 @@
 # @chopnico 2021
 
-# goenv exported variables
-export GOENV_ROOT="$HOME/.goenv"
-export PATH="$GOENV_ROOT/bin:$PATH"
+if [ -d "$HOME/.goenv" ]; then
+	# goenv exported variables
+	export GOENV_ROOT="$HOME/.goenv"
+	export PATH="$GOENV_ROOT/bin:$PATH"
 
-# Enables goenv shims
-eval "$(goenv init -)"
+	# Enables goenv shims
+	eval "$(goenv init -)"
 
-# Allows goenv to manage GOPATH and GOROOT
-export PATH="$GOROOT/bin:$PATH"
-export PATH="$PATH:$GOPATH/bin"
+	# Allows goenv to manage GOPATH and GOROOT
+	export PATH="$GOROOT/bin:$PATH"
+	export PATH="$PATH:$GOPATH/bin"
+fi
