98f7330077f4133ed8ac470ff84eec5cd1e8473a
Ajeet D'Souza <98ajeet@gmail.com>
2021-12-24 17:36:27 +0530

    Use type_exists
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

--- plugins/zoxide/zoxide.plugin.sh
+++ plugins/zoxide/zoxide.plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Check if zoxide is installed
-if [ -x "$(command -v zoxide)" ]; then
+if type_exists zoxide; then
     eval "$(zoxide init bash)"
 else
     echo '[oh-my-bash] zoxide not found, please install it from https://github.com/ajeetdsouza/zoxide'
