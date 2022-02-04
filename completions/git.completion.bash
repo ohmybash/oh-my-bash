c1065837227de0ec3885544b909f53e2feb29c50
Jean-Sébastien Hedde <jshedde@scaleway.com>
2022-02-04 16:48:34 +0100

    completions/git: Support multiple git paths and resolve symlink
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

--- completions/git.completion.bash
+++ completions/git.completion.bash
@@ -1,10 +1,17 @@
 # bash completion support for Git.
 _omb_completion_git_initialize() {
     if ! _omb_util_function_exists __gitdir; then
-        local path
-        for path in $(type -aP git);
-        do
-            path="${path%/git}"
+        local git_paths path
+        IFS=$'\n' read -r -d '' -a git_paths <<< "$(type -aP git)"
+        # Note: Falling back on /usr (should already be in the array)
+        git_paths+=("/usr/bin/git")
+        for path in "${git_paths[@]}"; do
+            if [[ -L $path ]]; then
+                path=$(readlink -f $path)
+            fi
+            # Note: In the case of symbolic link, the true binary name can
+            # contain prefix or suffix for architectures and versions.
+            path="${path%/*}"
             local prefix="${path%/bin}" file
             for file in share/bash-completion/completions/git share/git-core/contrib/completion/git-completion.bash;
             do
