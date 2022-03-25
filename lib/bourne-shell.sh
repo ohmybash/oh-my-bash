177864fc5c2428c1c636f075299cd705727cee06
mnlwldr <mnlwldr@gmail.com>
2022-03-25 09:05:09 +0100

    Added the possibility to have an own .bashrc.local

--- lib/bourne-shell.sh
+++ lib/bourne-shell.sh
@@ -90,6 +90,10 @@ if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
 
+if [ -f ~/.bashrc.local ]; then
+    . ~/.bashrc.local
+fi
+
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
