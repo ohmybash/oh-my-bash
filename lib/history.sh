5a7a9e3c65e0df11f3433087c62b04b1062ca099
Koichi Murase <myoga.murase@gmail.com>
2022-09-29 18:16:28 +0900

    lib/history (HIST_STAMPS): Adjust format

--- lib/history.sh
+++ lib/history.sh
@@ -54,7 +54,7 @@ bind '"\e[D": backward-char'
 # Show history
 case $HIST_STAMPS in
   "mm/dd/yyyy") HISTTIMEFORMAT=$'\033[31m[%m/%d/%Y] \033[36m[%T]\033[0m ' ;;
-  "dd.mm.yyyy") HISTTIMEFORMAT=$'\033[31m [%d.%m.%Y] \033[36m[%T]\033[0m ' ;;
-  "yyyy-mm-dd") HISTTIMEFORMAT=$'\033[31m%Y-%m-%d] \033[36m[%T]\033[0m ' ;;
+  "dd.mm.yyyy") HISTTIMEFORMAT=$'\033[31m[%d.%m.%Y] \033[36m[%T]\033[0m ' ;;
+  "yyyy-mm-dd") HISTTIMEFORMAT=$'\033[31m[%F] \033[36m[%T]\033[0m ' ;;
   *) HISTTIMEFORMAT=$'\033[31m%F \033[36m%T\033[0m ' ;;
 esac
