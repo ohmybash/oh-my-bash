2378cdf528fa9fe74d5fe78a46ba04c74f8d1524
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-10-24 13:25:49 +0200

    Terraform plugin

---
+++ plugins/terraform/README.md
@@ -0,0 +1,3 @@
+# Terraform Plugin
+
+The terraform plugin defines a number of useful aliases for you. [Consult the complete list](terraform.plugin.sh#L11)
---
+++ plugins/terraform/terraform.plugin.sh
@@ -0,0 +1,16 @@
+#! bash oh-my-bash.module
+#
+# Functions
+#
+
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+alias t='terraform'
+
+alias tapply='tapply'
+alias tfmt='terraform fmt'
+alias tinit='terraform init'
+alias tplan='terraform plan'
