1c7f6d67ced42b5aa4e6ab84bdba0980f3ad688a
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-11-06 04:11:39 +0100

    aliases: Add Terraform aliases (#367)

---
+++ aliases/terraform.aliases.sh
@@ -0,0 +1,10 @@
+# Aliases
+# (sorted alphabetically)
+#
+
+alias t='terraform'
+
+alias tapply='terraform apply'
+alias tfmt='terraform fmt'
+alias tinit='terraform init'
+alias tplan='terraform plan'
