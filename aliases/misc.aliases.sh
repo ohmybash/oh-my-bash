81099f1c285cae485bca66917c80ac63f8bb1f0f
Koichi Murase <myoga.murase@gmail.com>
2023-01-07 13:08:35 +0900

    aliases/misc: Do not assume the absolute path of executables

--- aliases/misc.aliases.sh
+++ aliases/misc.aliases.sh
@@ -63,21 +63,21 @@ alias qfind="find . -name "                 # qfind:    Quickly search for file
 #   4.  NETWORKING
 #   ---------------------------
 
-alias netCons='lsof -i'                                   # netCons:      Show all open TCP/IP sockets
-alias lsock='sudo /usr/sbin/lsof -i -P'                   # lsock:        Display open sockets
-alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP'         # lsockU:       Display only open UDP sockets
-alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'         # lsockT:       Display only open TCP sockets
-alias ipInfo0='ifconfig getpacket en0'                    # ipInfo0:      Get info on connections for en0
-alias ipInfo1='ifconfig getpacket en1'                    # ipInfo1:      Get info on connections for en1
-alias openPorts='sudo lsof -i | grep LISTEN'              # openPorts:    All listening connections
-alias showBlocked='sudo ipfw list'                        # showBlocked:  All ipfw rules inc/ blocked IPs
+alias netCons='lsof -i'                         # netCons:      Show all open TCP/IP sockets
+alias lsock='sudo lsof -i -P'                   # lsock:        Display open sockets
+alias lsockU='sudo lsof -nP | grep UDP'         # lsockU:       Display only open UDP sockets
+alias lsockT='sudo lsof -nP | grep TCP'         # lsockT:       Display only open TCP sockets
+alias ipInfo0='ifconfig getpacket en0'          # ipInfo0:      Get info on connections for en0
+alias ipInfo1='ifconfig getpacket en1'          # ipInfo1:      Get info on connections for en1
+alias openPorts='sudo lsof -i | grep LISTEN'    # openPorts:    All listening connections
+alias showBlocked='sudo ipfw list'              # showBlocked:  All ipfw rules inc/ blocked IPs
 
 
 #   ---------------------------------------
 #   5.  SYSTEMS OPERATIONS & INFORMATION
 #   ---------------------------------------
 
-alias mountReadWrite='/sbin/mount -uw /'    # mountReadWrite:   For use when booted into single-user
+alias mountReadWrite='mount -uw /'    # mountReadWrite:   For use when booted into single-user
 
 
 #   ---------------------------------------
