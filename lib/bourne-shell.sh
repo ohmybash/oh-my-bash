ce6e26ddeebde27027a027a45bc30c25567c56ef
Seth Underwood <underwoo@underwoo.io>
2023-01-06 21:36:36 -0500

    lib/bourne-shell: Add check for notify-send command prior to setting alert alias

--- lib/bourne-shell.sh
+++ lib/bourne-shell.sh
@@ -74,7 +74,9 @@ fi
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
-_omb_util_alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+if _omb_util_binary_exists notify-send; then
+  _omb_util_alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+fi
 
 # Alias definitions.
 # You may want to put all your additions into a separate file like
