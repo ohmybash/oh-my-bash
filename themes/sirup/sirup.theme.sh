71d9a1b7dac65c7bcda07f0befb0d8b04ac472d4
Koichi Murase <myoga.murase@gmail.com>
2023-02-12 15:41:57 +0900

    themes/sirup: Move segment "python_venv" next to segment "ruby_gem"

--- themes/sirup/sirup.theme.sh
+++ themes/sirup/sirup.theme.sh
@@ -1,8 +1,8 @@
 #! bash oh-my-bash.module
 # For unstaged(*) and staged(+) values next to branch name in __git_ps1
 GIT_PS1_SHOWDIRTYSTATE="enabled"
-OMB_PROMPT_VIRTUALENV_FORMAT='[%s] '
-OMB_PROMPT_CONDAENV_FORMAT='[%s] '
+OMB_PROMPT_VIRTUALENV_FORMAT=' [%s]'
+OMB_PROMPT_CONDAENV_FORMAT=' [%s]'
 OMB_PROMPT_SHOW_PYTHON_VENV=${OMB_PROMPT_SHOW_PYTHON_VENV:=false}
 
 function _omb_theme_sirup_rubygem {
@@ -20,7 +20,7 @@ function _omb_theme_PROMPT_COMMAND {
   # Check http://github.com/Sirupsen/dotfiles for screenshot
   local python_venv
   _omb_prompt_get_python_venv
-  PS1="$_omb_prompt_purple$python_venv$_omb_prompt_navy\W/$_omb_prompt_bold_navy$(_omb_theme_sirup_rubygem)$_omb_prompt_bold_green$(__git_ps1 " (%s)") ${_omb_prompt_normal}$ "
+  PS1="$_omb_prompt_navy\W/$_omb_prompt_bold_navy$(_omb_theme_sirup_rubygem)$_omb_prompt_bold_purple$python_venv$_omb_prompt_bold_green$(__git_ps1 " (%s)") ${_omb_prompt_normal}$ "
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
