70e078751051c8dec5a8bcc89b0200226cabe6e6
Koichi Murase <myoga.murase@gmail.com>
2023-03-13 00:09:21 +0900

    completions/git: Search upstream git-completion files in more paths (Fix #415)
    https://github.com/ohmybash/oh-my-bash/issues/415

--- completions/git.completion.bash
+++ completions/git.completion.bash
@@ -14,7 +14,7 @@ function _omb_completion_git_initialize {
             # contain prefix or suffix for architectures and versions.
             path="${path%/*}"
             local files
-            _omb_util_glob_expand files '"$prefix"/share/{bash-completion/completions/git,git-core/contrib/completion/git-completion.bash}'
+            _omb_util_glob_expand files '"$prefix"/share/{bash-completion/completions/git,git-*/contrib/completion/git-completion.bash}'
             local prefix="${path%/bin}" file
             for file in "${files[@]}"; do
                 if [[ -f $file && -r $file && -s $file ]]; then
