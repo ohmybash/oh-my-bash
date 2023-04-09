5be9ad0c7c356e2be18ac845d90ffd08501f5fe4
Dheison Gomes <dheisomgomes0@gmail.com>
2023-04-09 20:00:22 -0300

    Add support to toggle and fix a bug when use 4 <esc> press

--- plugins/sudo/sudo.plugin.sh
+++ plugins/sudo/sudo.plugin.sh
@@ -1,11 +1,21 @@
 #! bash oh-my-bash.module
 
 function _omb_plugin_sudo_add_sudo {
+  function __omb_plugin_sudo_is_inserted {
+    [[ "${READLINE_LINE::4}" = "sudo" ]]; return $?
+  }
   if [[ -z $READLINE_LINE ]]; then
     READLINE_LINE="$(fc -ln -1 | command sed 's/^[[:space:]]\{1,\}//')"
+    ((READLINE_POINT += ${#READLINE_LINE}))
+    __omb_plugin_sudo_is_inserted && return
   fi
-  READLINE_LINE="sudo $READLINE_LINE"
-  ((READLINE_POINT += 5))
+  if __omb_plugin_sudo_is_inserted; then
+    READLINE_LINE="${READLINE_LINE//sudo /}"
+    ((READLINE_POINT -= 5))
+  else
+    READLINE_LINE="sudo $READLINE_LINE"
+    ((READLINE_POINT += 5))
+  fi
 }
 
 bind -m emacs -x '"\e\e": _omb_plugin_sudo_add_sudo'
