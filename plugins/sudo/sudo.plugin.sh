6ef62dd4c25c95d8bfa413cb0e9228b864ced7bd
Dheison Gomes <dheisomgomes0@gmail.com>
2023-04-09 21:16:39 -0300

    Changes applied and tested

--- plugins/sudo/sudo.plugin.sh
+++ plugins/sudo/sudo.plugin.sh
@@ -1,21 +1,23 @@
 #! bash oh-my-bash.module
 
+function _omb_plugin_sudo__is_inserted {
+  [[ "${READLINE_LINE::5}" = "sudo " ]]
+}
+
 function _omb_plugin_sudo_add_sudo {
-  function __omb_plugin_sudo_is_inserted {
-    [[ "${READLINE_LINE::4}" = "sudo" ]]; return $?
-  }
   if [[ -z $READLINE_LINE ]]; then
     READLINE_LINE="$(fc -ln -1 | command sed 's/^[[:space:]]\{1,\}//')"
-    ((READLINE_POINT += ${#READLINE_LINE}))
-    __omb_plugin_sudo_is_inserted && return
+    READLINE_POINT=${#READLINE_LINE}
+    _omb_plugin_sudo__is_inserted && return
   fi
-  if __omb_plugin_sudo_is_inserted; then
-    READLINE_LINE="${READLINE_LINE//sudo /}"
+  if _omb_plugin_sudo__is_inserted; then
+    READLINE_LINE="${READLINE_LINE#sudo }"
     ((READLINE_POINT -= 5))
   else
     READLINE_LINE="sudo $READLINE_LINE"
     ((READLINE_POINT += 5))
   fi
+  ((READLINE_POINT < 0)) && READLINE_POINT=0
 }
 
 bind -m emacs -x '"\e\e": _omb_plugin_sudo_add_sudo'
