e6572f4b8561c7f89daf8bc58e6758a4f4b65d96
Koichi Murase <myoga.murase@gmail.com>
2023-10-01 03:32:03 +0900

    completions/{cap,homesick,rake,sdkman,ssh}: Change indentation and whitespaces

--- completions/homesick.completion.sh
+++ completions/homesick.completion.sh
@@ -6,55 +6,55 @@
 
 _omb_completion_homesick()
 {
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    local options="--skip --force --pretend --quiet"
-    local actions="cd clone commit destroy diff generate help list open pull push rc show_path status symlink track unlink version"
-    local repos=$(\ls ~/.homesick/repos)
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local options="--skip --force --pretend --quiet"
+  local actions="cd clone commit destroy diff generate help list open pull push rc show_path status symlink track unlink version"
+  local repos=$(\ls ~/.homesick/repos)
 
-    # Subcommand list
-    [[ ${COMP_CWORD} -eq 1 ]] && {
-        COMPREPLY=( $(compgen -W "${options} ${actions}" -- ${cur}) )
-        return
-    }
+  # Subcommand list
+  [[ ${COMP_CWORD} -eq 1 ]] && {
+    COMPREPLY=( $(compgen -W "${options} ${actions}" -- ${cur}) )
+    return
+  }
 
-    # Find the first non-switch word
-    local prev_index=1
-    local prev="${COMP_WORDS[prev_index]}"
-    while [[ $prev == -* ]]; do
-        prev_index=$((++prev_index))
-        prev="${COMP_WORDS[prev_index]}"
-    done
+  # Find the first non-switch word
+  local prev_index=1
+  local prev="${COMP_WORDS[prev_index]}"
+  while [[ $prev == -* ]]; do
+    prev_index=$((++prev_index))
+    prev="${COMP_WORDS[prev_index]}"
+  done
 
-    # Find the number of non-"--" commands
-    local num=0
-    for word in ${COMP_WORDS[@]}
-    do
-        if [[ $word != -* ]]; then
-            num=$((++num))
-        fi
-    done
+  # Find the number of non-"--" commands
+  local num=0
+  for word in ${COMP_WORDS[@]}
+  do
+    if [[ $word != -* ]]; then
+      num=$((++num))
+    fi
+  done
 
-    case "$prev" in
-    # Commands that take a castle
-    cd|commit|destroy|diff|open|pull|push|rc|show_path|status|symlink|unlink)
-        COMPREPLY=( $(compgen -W "${repos}" -- ${cur}) )
-        return
-        ;;
-    # Commands that take command
-    help)
-        COMPREPLY=( $(compgen -W "${actions}" -- ${cur}) )
-        return
-        ;;
-    # Track command take file and repo
-    track)
-        if [[ "$num" -eq 2 ]]; then
-            COMPREPLY=( $(compgen -X -f ${cur}) )
-        elif [[ "$num" -ge 3 ]]; then
-            COMPREPLY=( $(compgen -W "${repos}" -- ${cur}) )
-        fi
-        return
-        ;;
-    esac
+  case "$prev" in
+  # Commands that take a castle
+  cd|commit|destroy|diff|open|pull|push|rc|show_path|status|symlink|unlink)
+    COMPREPLY=( $(compgen -W "${repos}" -- ${cur}) )
+    return
+    ;;
+  # Commands that take command
+  help)
+    COMPREPLY=( $(compgen -W "${actions}" -- ${cur}) )
+    return
+    ;;
+  # Track command take file and repo
+  track)
+    if [[ "$num" -eq 2 ]]; then
+      COMPREPLY=( $(compgen -X -f ${cur}) )
+    elif [[ "$num" -ge 3 ]]; then
+      COMPREPLY=( $(compgen -W "${repos}" -- ${cur}) )
+    fi
+    return
+    ;;
+  esac
 }
 
 complete -o bashdefault -o default -F _omb_completion_homesick homesick
--- completions/rake.completion.sh
+++ completions/rake.completion.sh
@@ -4,14 +4,14 @@
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
 function _omb_completion_rake {
-    if [ -f Rakefile ]; then
-        recent=`ls -t .rake_tasks~ Rakefile **/*.rake 2> /dev/null | head -n 1`
-        if [[ $recent != '.rake_tasks~' ]]; then
-            rake --silent --tasks --all | cut -d " " -f 2 > .rake_tasks~
-        fi
-        COMPREPLY=($(compgen -W "`cat .rake_tasks~`" -- ${COMP_WORDS[COMP_CWORD]}))
-        return 0
+  if [ -f Rakefile ]; then
+    recent=`ls -t .rake_tasks~ Rakefile **/*.rake 2> /dev/null | head -n 1`
+    if [[ $recent != '.rake_tasks~' ]]; then
+      rake --silent --tasks --all | cut -d " " -f 2 > .rake_tasks~
     fi
+    COMPREPLY=($(compgen -W "`cat .rake_tasks~`" -- ${COMP_WORDS[COMP_CWORD]}))
+    return 0
+  fi
 }
 
 complete -o default -o nospace -F _omb_completion_rake rake
