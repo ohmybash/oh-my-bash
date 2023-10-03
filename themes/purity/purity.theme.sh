1975f1dc6d19c5d7ee434e937fcb462b6151aca7
Koichi Murase <myoga.murase@gmail.com>
2023-10-04 06:58:26 +0900

    themes/purity: Apply refactoring in edsonarios

--- themes/purity/purity.theme.sh
+++ themes/purity/purity.theme.sh
@@ -13,8 +13,12 @@ GIT_THEME_PROMPT_SUFFIX=" ${_omb_prompt_reset_color})"
 STATUS_THEME_PROMPT_BAD="${_omb_prompt_bold_brown}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
 STATUS_THEME_PROMPT_OK="${_omb_prompt_bold_green}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
 
-function _omb_theme_PROMPT_COMMAND() {
-  local ret_status="$( [ $? -eq 0 ] && echo -e "$STATUS_THEME_PROMPT_OK" || echo -e "$STATUS_THEME_PROMPT_BAD")"
+function _omb_theme_PROMPT_COMMAND {
+  if (($? == 0)); then
+    local ret_status=${STATUS_THEME_PROMPT_BAD-}
+  else
+    local ret_status=${STATUS_THEME_PROMPT_OK-}
+  fi
   PS1="\n${_omb_prompt_navy}\w $(scm_prompt_info)\n${ret_status} "
 }
 
