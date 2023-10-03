446a19072f96f1430b0e927e13d9f8ff0f3a8c75
Koichi Murase <myoga.murase@gmail.com>
2023-10-04 06:53:01 +0900

    themes/purity: Change indentation

--- themes/purity/purity.theme.sh
+++ themes/purity/purity.theme.sh
@@ -14,8 +14,8 @@ STATUS_THEME_PROMPT_BAD="${_omb_prompt_bold_brown}❯${_omb_prompt_reset_color}$
 STATUS_THEME_PROMPT_OK="${_omb_prompt_bold_green}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
 
 function _omb_theme_PROMPT_COMMAND() {
-    local ret_status="$( [ $? -eq 0 ] && echo -e "$STATUS_THEME_PROMPT_OK" || echo -e "$STATUS_THEME_PROMPT_BAD")"
-    PS1="\n${_omb_prompt_navy}\w $(scm_prompt_info)\n${ret_status} "
+  local ret_status="$( [ $? -eq 0 ] && echo -e "$STATUS_THEME_PROMPT_OK" || echo -e "$STATUS_THEME_PROMPT_BAD")"
+  PS1="\n${_omb_prompt_navy}\w $(scm_prompt_info)\n${ret_status} "
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
