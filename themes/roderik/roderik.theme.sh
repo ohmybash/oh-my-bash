0a7a083308b1c4a57a8fc8c7a8ebe33f44d1d377
Koichi Murase <myoga.murase@gmail.com>
2023-10-10 12:21:52 +0900

    themes/roderik: Change indentation

--- themes/roderik/roderik.theme.sh
+++ themes/roderik/roderik.theme.sh
@@ -7,11 +7,11 @@ export GIT_PS1_SHOWSTASHSTATE=true
 export PROMPT_DIRTRIM=${PROMPT_DIRTRIM:-3}
 
 function _omb_theme_PROMPT_COMMAND() {
-    if [[ ${EUID} == 0 ]] ; then
-        PS1="[$(clock_prompt)]${_omb_prompt_olive}[${_omb_prompt_brown}\u@\h ${_omb_prompt_green}\w${_omb_prompt_olive}]${_omb_prompt_brown}$(__git_ps1 "(%s)")${_omb_prompt_normal}\\$ "
-    else
-        PS1="[$(clock_prompt)]${_omb_prompt_olive}[${_omb_prompt_teal}\u@\h ${_omb_prompt_green}\w${_omb_prompt_olive}]${_omb_prompt_brown}$(__git_ps1 "(%s)")${_omb_prompt_normal}\\$ "
-    fi
+  if [[ ${EUID} == 0 ]] ; then
+    PS1="[$(clock_prompt)]${_omb_prompt_olive}[${_omb_prompt_brown}\u@\h ${_omb_prompt_green}\w${_omb_prompt_olive}]${_omb_prompt_brown}$(__git_ps1 "(%s)")${_omb_prompt_normal}\\$ "
+  else
+    PS1="[$(clock_prompt)]${_omb_prompt_olive}[${_omb_prompt_teal}\u@\h ${_omb_prompt_green}\w${_omb_prompt_olive}]${_omb_prompt_brown}$(__git_ps1 "(%s)")${_omb_prompt_normal}\\$ "
+  fi
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
