0ff46f6d32e2e9387061dc5f1a6e50a773e84b83
Koichi Murase <myoga.murase@gmail.com>
2023-10-12 15:20:52 +0900

    plugins/bashmarks: Fix licensing issue

--- plugins/bashmarks/bashmarks.plugin.sh
+++ plugins/bashmarks/bashmarks.plugin.sh
@@ -1,27 +1,39 @@
 #! bash oh-my-bash.module
-# Copyright (c) 2015, Toan Nguyen - https://nntoan.github.io
+# Copyright (c) 2010, Huy Nguyen, https://everyhue.me
+# Copyright (c) 2015, Toan Nguyen, https://nntoan.github.io
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided
-# that the following conditions are met:
+# This plugin is derived from the project https://github.com/huing/bashmarks.
+# This version is based on the following commit in the upstream project:
 #
-#     * Redistributions of source code must retain the above copyright notice, this list of conditions
-#       and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-#       following disclaimer in the documentation and/or other materials provided with the distribution.
-#     * Neither the name of Toan Nguyen Ngoc (aka NNToan) nor the names of contributors
-#       may be used to endorse or promote products derived from this software without
+#   https://github.com/huyng/bashmarks/commit/003bc0702684cf59802d3400ba2485fa3d378e01
+#
+# This plugin is licensed under the BSD-3 License.
+#------------------------------------------------------------------------------
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Huy Nguyen nor the names of contributors may be
+#       used to endorse or promote products derived from this software without
 #       specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
+#------------------------------------------------------------------------------
 
 # USAGE:
 # bm -a bookmarkname - saves the curr dir as bookmarkname
