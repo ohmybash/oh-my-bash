c78bc44cf9cef57b80632fce37c77ebcbbad8bcd
Yishuai Li <liyishuai.lys@alibaba-inc.com>
2023-11-22 15:54:53 +0800

    themes/random: Blacklisting Themes

--- oh-my-bash.sh
+++ oh-my-bash.sh
@@ -144,6 +144,18 @@ unset -v _omb_init_files _omb_init_file
 # Load the theme
 if [[ $OSH_THEME == random ]]; then
   _omb_util_glob_expand _omb_init_files '"$OSH"/themes/*/*.theme.sh'
+
+  # Remove ignored themes from the list
+  for _omb_init_theme in random "${OMB_THEME_RANDOM_IGNORED[@]}"; do
+    for _omb_init_index in "${!_omb_init_files[@]}"; do
+      [[ ${_omb_init_files[_omb_init_index]} == */"$_omb_init_theme"/* ]] &&
+        unset -v '_omb_init_files[_omb_init_index]'
+    done
+    unset -v _omb_init_index
+  done
+  unset -v _omb_init_theme
+  _omb_init_files=("${_omb_init_files[@]}")
+
   if ((${#_omb_init_files[@]})); then
     _omb_init_file=${_omb_init_files[RANDOM%${#_omb_init_files[@]}]}
     source "$_omb_init_file"
