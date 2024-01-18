4738fef72c3b26d5898b4c4ddd5cc38b8a6814e7
TheWatcherMultiversal <thewatcherjudge@protonmail.com>
2024-01-18 05:13:58 -0500

    themes/powerline-icon: Fix icon for successful execution (#516)
    * themes/powerline-icon: Correct error
    
    An error in one of the powerline functions has been corrected, which
    was preventing the display of the icon for a successfully executed
    command.
    
    * themes/THEMES: Add powerline-icon

--- themes/powerline/powerline.base.sh
+++ themes/powerline/powerline.base.sh
@@ -192,7 +192,11 @@ function __powerline_prompt_command {
     local info="$(__powerline_${segment}_prompt)"
     [[ -n "${info}" ]] && __powerline_left_segment "${info}"
   done
-  [[ "${last_status}" -ne 0 ]] && __powerline_left_segment $(__powerline_last_status_prompt ${last_status})
+
+  ## info status prompt ##
+  local info="$(__powerline_last_status_prompt ${last_status})"
+  [[ -n "${info}" ]] && __powerline_left_segment "${info}"
+
   [[ -n "${LEFT_PROMPT}" ]] && LEFT_PROMPT+="$(set_color ${LAST_SEGMENT_COLOR} -)${separator_char}${_omb_prompt_normal}"
 
   PS1="${LEFT_PROMPT} "
