3b120c1dc4398c11ccd20f349720832e698f88ab
Koichi Murase <myoga.murase@gmail.com>
2024-05-02 09:28:59 +0900

    style(themes/wanelo): adjust styles
    * style(themes/wanelo): fix indentation
    * style(themes/wanelo): use arithmetic command
    * style(themes/wanelo): use "function func { ... }"

--- themes/wanelo/wanelo.theme.sh
+++ themes/wanelo/wanelo.theme.sh
@@ -12,13 +12,13 @@ GIT_THEME_PROMPT_SUFFIX="${_omb_prompt_green}|"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
-function _omb_theme_PROMPT_COMMAND() {
-    if [ $? -eq 0 ]; then
-      status=❤️
-    else
-      status=💔
-    fi
-    PS1="\n${_omb_prompt_olive}$(_omb_prompt_print_ruby_env) ${_omb_prompt_purple}\h ${_omb_prompt_reset_color}in ${_omb_prompt_green}\w $status \n${_omb_prompt_bold_teal} ${_omb_prompt_navy}|$(clock_prompt)|${_omb_prompt_green}$(scm_prompt_info) ${_omb_prompt_green}→${_omb_prompt_reset_color} "
+function _omb_theme_PROMPT_COMMAND {
+  if (($? == 0)); then
+    status=❤️
+  else
+    status=💔
+  fi
+  PS1="\n${_omb_prompt_olive}$(_omb_prompt_print_ruby_env) ${_omb_prompt_purple}\h ${_omb_prompt_reset_color}in ${_omb_prompt_green}\w $status \n${_omb_prompt_bold_teal} ${_omb_prompt_navy}|$(clock_prompt)|${_omb_prompt_green}$(scm_prompt_info) ${_omb_prompt_green}→${_omb_prompt_reset_color} "
 }
 
 THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$_omb_prompt_navy"}
