fcce66a85c4fa510bfb3c3feb0f4cbe76f3e1bd5
Koichi Murase <myoga.murase@gmail.com>
2024-05-16 18:01:49 +0900

    fix(completions/system): load bash-completion only once
    We have been sourcing bash-completion as many times as the number of
    the installations and entry points that we can find.  However, only
    one instance of bash-completion is enough.
    
    We also re-order the detection to adjust the precedence.

--- completions/system.completion.sh
+++ completions/system.completion.sh
@@ -1,27 +1,31 @@
 #! bash oh-my-bash.module
 
-# Loads the system's Bash completion modules.
-# If Homebrew is installed (OS X), its Bash completion modules are loaded.
+# Loads one of the system's Bash-completion modules.
 
-if [[ -f /etc/bash_completion ]]; then
-  source /etc/bash_completion
-fi
-
-# Some distribution makes use of a profile.d script to import completion.
-if [[ -f /etc/profile.d/bash_completion.sh ]]; then
-  source /etc/profile.d/bash_completion.sh
-fi
-
-
+# If Homebrew is installed (OS X), its Bash completion module is loaded.
 if is_os darwin && _omb_util_command_exists brew; then
   BREW_PREFIX=$(brew --prefix)
 
-  if [[ -f "$BREW_PREFIX"/etc/bash_completion ]]; then
-    source "$BREW_PREFIX"/etc/bash_completion
-  fi
-
-  # homebrew/versions/bash-completion2 (required for projects.completion.bash) is installed to this path
+  # homebrew/versions/bash-completion2 (required for projects.completion.bash)
+  # is installed to this path
   if [[ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]]; then
     source "$BREW_PREFIX"/share/bash-completion/bash_completion
+    return "$?"
   fi
+
+  if [[ -f "$BREW_PREFIX"/etc/bash_completion ]]; then
+    source "$BREW_PREFIX"/etc/bash_completion
+    return "$?"
+  fi
+fi
+
+if [[ -f /etc/bash_completion ]]; then
+  source /etc/bash_completion
+  return "$?"
+fi
+
+if [[ -f /etc/profile.d/bash_completion.sh ]]; then
+  # Some distribution makes use of a profile.d script to import completion.
+  source /etc/profile.d/bash_completion.sh
+  return "$?"
 fi
