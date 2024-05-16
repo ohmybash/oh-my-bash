ebb8c36fc791c8d6005401d232771b79eb5a782f
Barak Korren <bkorren@redhat.com>
2024-05-16 11:03:24 +0300

    lib/bourne-shell: Do not load bash-completion
    It's already done better by the "system" completion module, and doing
    it there too makes disabling it impossible and debgging issues harder.

--- lib/bourne-shell.sh
+++ lib/bourne-shell.sh
@@ -98,14 +98,3 @@ fi
 if [ -f ~/.bashrc.local ]; then
     . ~/.bashrc.local
 fi
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
-fi
