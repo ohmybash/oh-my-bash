3a0381401be0701bdcfdbe0ed28a9abe14ec3565
Koichi Murase <myoga.murase@gmail.com>
2024-05-16 17:53:10 +0900

    style(completions/system): adjust coding styles

--- completions/system.completion.sh
+++ completions/system.completion.sh
@@ -3,25 +3,25 @@
 # Loads the system's Bash completion modules.
 # If Homebrew is installed (OS X), its Bash completion modules are loaded.
 
-if [ -f /etc/bash_completion ]; then
-  . /etc/bash_completion
+if [[ -f /etc/bash_completion ]]; then
+  source /etc/bash_completion
 fi
 
 # Some distribution makes use of a profile.d script to import completion.
-if [ -f /etc/profile.d/bash_completion.sh ]; then
-  . /etc/profile.d/bash_completion.sh
+if [[ -f /etc/profile.d/bash_completion.sh ]]; then
+  source /etc/profile.d/bash_completion.sh
 fi
 
 
-if [ $(uname) = "Darwin" ] && _omb_util_command_exists brew; then
+if [[ $(uname) == "Darwin" ]] && _omb_util_command_exists brew; then
   BREW_PREFIX=$(brew --prefix)
 
-  if [ -f "$BREW_PREFIX"/etc/bash_completion ]; then
-    . "$BREW_PREFIX"/etc/bash_completion
+  if [[ -f "$BREW_PREFIX"/etc/bash_completion ]]; then
+    source "$BREW_PREFIX"/etc/bash_completion
   fi
 
   # homebrew/versions/bash-completion2 (required for projects.completion.bash) is installed to this path
-  if [ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]; then
-    . "$BREW_PREFIX"/share/bash-completion/bash_completion
+  if [[ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]]; then
+    source "$BREW_PREFIX"/share/bash-completion/bash_completion
   fi
 fi
