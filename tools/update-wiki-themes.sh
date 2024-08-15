85d425a05775cdd249d87824ed404f573fedb234
Koichi Murase <myoga.murase@gmail.com>
2024-08-15 16:46:33 +0900

    style(tools/update-wiki-themes): update coding styles

--- tools/update-wiki-themes.sh
+++ tools/update-wiki-themes.sh
@@ -28,72 +28,72 @@
 
 # first process current env vars, with some sensible default fallback values...
 
-SCRIPT_WIKI_PATH="${OMB_WIKI_PATH:-../oh-my-bash.wiki}"
-SCRIPT_WIKI_THEMES_FILE="${OMB_WIKI_THEMES_FILE:-Themes.md}"
-SCRIPT_WIKI_THEMES_START_MARKER="${OMB_WIKI_THEMES_START_MARKER:-<!-- THEME_GEN_START_MARKER -->}"
-SCRIPT_WIKI_THEMES_END_MARKER="${OMB_WIKI_THEMES_END_MARKER:-<!-- THEME_GEN_END_MARKER -->}"
+SCRIPT_WIKI_PATH=${OMB_WIKI_PATH:-../oh-my-bash.wiki}
+SCRIPT_WIKI_THEMES_FILE=${OMB_WIKI_THEMES_FILE:-Themes.md}
+SCRIPT_WIKI_THEMES_START_MARKER=${OMB_WIKI_THEMES_START_MARKER:-'<!-- THEME_GEN_START_MARKER -->'}
+SCRIPT_WIKI_THEMES_END_MARKER=${OMB_WIKI_THEMES_END_MARKER:-'<!-- THEME_GEN_END_MARKER -->'}
 
 # then process any cli args, if provided...
 
-VALID_ARGS=$(getopt -o p:f:s:e: --long wiki-path:,themes-file:,start-marker:,end-marker: -- "$@")
-if [[ $? -ne 0 ]]; then
-  exit 1;
+if ! VALID_ARGS=$(getopt -o p:f:s:e: --long wiki-path:,themes-file:,start-marker:,end-marker: -- "$@"); then
+  exit 2
 fi
 
-eval set -- "$VALID_ARGS"
-while [ : ]; do
-  case "$1" in
+eval "set -- $VALID_ARGS"
+while (($#)); do
+  case $1 in
   -p | --wiki-path)
     # echo "Processing 'wiki-path' option. Input argument is '$2'"
-    SCRIPT_WIKI_PATH="$2"
+    SCRIPT_WIKI_PATH=$2
     shift 2
     ;;
   -f | --themes-file)
     # echo "Processing 'themes-file' option. Input argument is '$2'"
-    SCRIPT_WIKI_THEMES_FILE="$2"
+    SCRIPT_WIKI_THEMES_FILE=$2
     shift 2
     ;;
   -s | --start-marker)
     # echo "Processing 'start-marker' option. Input argument is '$2'"
-    SCRIPT_WIKI_THEMES_START_MARKER="$2"
+    SCRIPT_WIKI_THEMES_START_MARKER=$2
     shift 2
     ;;
   -e | --end-marker)
     # echo "Processing 'end-marker' option. Input argument is '$2'"
-    SCRIPT_WIKI_THEMES_END_MARKER="$2"
+    SCRIPT_WIKI_THEMES_END_MARKER=$2
     shift 2
     ;;
-  --) shift;
-      break
-      ;;
+  --)
+    shift
+    break
+    ;;
   esac
 done
 
 # debug: this will either be adapted for the final script, or removed entirely..
-echo "current SCRIPT_WIKI_PATH: $SCRIPT_WIKI_PATH"
-echo "current SCRIPT_WIKI_THEMES_FILE: $SCRIPT_WIKI_THEMES_FILE"
-echo "current SCRIPT_WIKI_THEMES_START_MARKER: $SCRIPT_WIKI_THEMES_START_MARKER"
-echo "current SCRIPT_WIKI_THEMES_END_MARKER: $SCRIPT_WIKI_THEMES_END_MARKER"
+printf '%s\n' "current SCRIPT_WIKI_PATH: $SCRIPT_WIKI_PATH"
+printf '%s\n' "current SCRIPT_WIKI_THEMES_FILE: $SCRIPT_WIKI_THEMES_FILE"
+printf '%s\n' "current SCRIPT_WIKI_THEMES_START_MARKER: $SCRIPT_WIKI_THEMES_START_MARKER"
+printf '%s\n' "current SCRIPT_WIKI_THEMES_END_MARKER: $SCRIPT_WIKI_THEMES_END_MARKER"
 
 # verify that the OMB Wiki project exists...
-if [[ ! -d "$SCRIPT_WIKI_PATH" ]]; then
-  echo "ERROR: Wiki project path '$SCRIPT_WIKI_PATH' does not exist."
-  exit 1;
+if [[ ! -d $SCRIPT_WIKI_PATH ]]; then
+  printf '%s\n' "ERROR: Wiki project path '$SCRIPT_WIKI_PATH' does not exist."
+  exit 1
 fi
 
 # verify that the OMB Wiki contains the expected themes file...
-if [[ ! -f "$SCRIPT_WIKI_PATH/$SCRIPT_WIKI_THEMES_FILE" ]]; then
-  echo "ERROR: Wiki project has no themes file called '$SCRIPT_WIKI_THEMES_FILE'."
-  exit 1;
+if [[ ! -f $SCRIPT_WIKI_PATH/$SCRIPT_WIKI_THEMES_FILE ]]; then
+  printf '%s\n' "ERROR: Wiki project has no themes file called '$SCRIPT_WIKI_THEMES_FILE'."
+  exit 1
 fi
 
 # verify that the OMB Wiki contains the expected start and end markers...
-if ! (grep -q "$SCRIPT_WIKI_THEMES_START_MARKER" "$SCRIPT_WIKI_PATH/$SCRIPT_WIKI_THEMES_FILE"); then
-  echo "ERROR: Wiki themes file does not contain start marker '$SCRIPT_WIKI_THEMES_START_MARKER'."
+if ! grep -q "$SCRIPT_WIKI_THEMES_START_MARKER" "$SCRIPT_WIKI_PATH/$SCRIPT_WIKI_THEMES_FILE"; then
+  printf '%s\n' "ERROR: Wiki themes file does not contain start marker '$SCRIPT_WIKI_THEMES_START_MARKER'."
   exit 1
 fi
-if ! (grep -q "$SCRIPT_WIKI_THEMES_END_MARKER" "$SCRIPT_WIKI_PATH/$SCRIPT_WIKI_THEMES_FILE"); then
-  echo "ERROR: Wiki themes file does not contain end marker '$SCRIPT_WIKI_THEMES_END_MARKER'."
+if ! grep -q "$SCRIPT_WIKI_THEMES_END_MARKER" "$SCRIPT_WIKI_PATH/$SCRIPT_WIKI_THEMES_FILE"; then
+  printf '%s\n' "ERROR: Wiki themes file does not contain end marker '$SCRIPT_WIKI_THEMES_END_MARKER'."
   exit 1
 fi
 
@@ -107,21 +107,21 @@ markdown_text="$SCRIPT_WIKI_THEMES_START_MARKER\n\n"
 
 # now we can loop through the list and find all images in each theme directory...
 for theme in $theme_list; do
-  theme_dir="./themes/${theme}"
+  theme_dir=./themes/$theme
   image_list=$(find "$theme_dir" -type f -name "*.png" -o -name "*.jpg")
 
   # start preparing a theme example markdown block...
-  markdown_text="${markdown_text}## \`${theme}\`\n\n"
+  markdown_text="$markdown_text## \`$theme\`\n\n"
 
   # loop through the image list and add each image to the theme example entry...
-  if [[ ! -z "$image_list" ]]; then
+  if [[ ! -z $image_list ]]; then
     for image in $image_list; do
       # Extract filename and construct image URL
       image_filename=$(basename "$image")
-      image_url="https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/themes/$theme/$image_filename"
+      image_url=https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/themes/$theme/$image_filename
 
       # append image to theme example markdown block...
-      markdown_text="${markdown_text}![](${image_url})\n"
+      markdown_text="$markdown_text![]($image_url)\n"
     done
   else
     markdown_text="${markdown_text}WARNING: theme contains no example images.\n"
@@ -132,7 +132,7 @@ for theme in $theme_list; do
 done
 
 # inject the "end" marker for next run...
-markdown_text="$markdown_text$SCRIPT_WIKI_THEMES_END_MARKER"
+markdown_text=$markdown_text$SCRIPT_WIKI_THEMES_END_MARKER
 
 # now we can update the OMB Wiki "Themes" page directly...
 sed -i "/$SCRIPT_WIKI_THEMES_START_MARKER/,/$SCRIPT_WIKI_THEMES_END_MARKER/c\\
