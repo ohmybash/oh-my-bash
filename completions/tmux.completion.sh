ohmybash/oh-my-bash

commit 52987b3d35f00bf012263698fd4120677aa7c1dc

style(completions/...): fix styles
* style(completions/defaults): fix styles
* style(completions/go): adjust styles
* style(completions/gradle): adjust styles
* style(completions/grunt): adjust styles
* style(completions/maven): adjust styles
* style(completions/salt): adjust styles
* style(completions/sdkman): adjust styles
* style(completions/tmux): adjust styles
* style(completions/vagrant): adjust styles
* style(completions/vault): adjust styles

completions/tmux.completion.sh
@@ -5,56 +5,51 @@
 # Usage: Put "source bash_completion_tmux.sh" into your .bashrc
 # Based upon the example at http://paste-it.appspot.com/Pj4mLycDE
 
-_tmux_expand ()
-{
-  [ "$cur" != "${cur%\\}" ] && cur="$cur"'\';
-  if [[ "$cur" == \~*/* ]]; then
-    eval cur=$cur;
-  else
-    if [[ "$cur" == \~* ]]; then
-      cur=${cur#\~};
-      COMPREPLY=($( compgen -P '~' -u $cur ));
-      return ${#COMPREPLY[@]};
-    fi;
+function _tmux_expand {
+  [[ $cur != "${cur%\\}" ]] && cur=$cur'\'
+  if [[ $cur == \~*/* ]]; then
+    eval "cur=$cur"
+  elif [[ $cur == \~* ]]; then
+    cur=${cur#\~}
+    COMPREPLY=($(compgen -P '~' -u "$cur"))
+    ((${#COMPREPLY[@]} == 0))
   fi
 }
 
-_tmux_filedir ()
-{
-  local IFS='
-';
-  _tmux_expand || return 0;
-  if [ "$1" = -d ]; then
-    COMPREPLY=(${COMPREPLY[@]} $( compgen -d -- $cur ));
+function _tmux_filedir {
+  local IFS=$'\n'
+  _tmux_expand || return 0
+  if [[ $1 == -d ]]; then
+    COMPREPLY=("${COMPREPLY[@]}" $(compgen -d -- "$cur"));
     return 0;
   fi;
-  COMPREPLY=(${COMPREPLY[@]} $( eval compgen -f -- \"$cur\" ))
+  COMPREPLY=("${COMPREPLY[@]}" $(eval "compgen -f -- \"$cur\""))
 }
 
-function _tmux_complete_client() {
+function _tmux_complete_client {
   local IFS=$'\n'
-  local cur="${1}"
-  COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "$(tmux -q list-clients 2>/dev/null | cut -f 1 -d ':')" -- "${cur}") )
+  local cur=$1
+  COMPREPLY=("${COMPREPLY[@]}" $(compgen -W "$(tmux -q list-clients 2>/dev/null | cut -f 1 -d ':')" -- "${cur}") )
 }
-function _tmux_complete_session() {
+function _tmux_complete_session {
   local IFS=$'\n'
-  local cur="${1}"
-  COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "$(tmux -q list-sessions 2>/dev/null | cut -f 1 -d ':')" -- "${cur}") )
+  local cur=$1
+  COMPREPLY=("${COMPREPLY[@]}" $(compgen -W "$(tmux -q list-sessions 2>/dev/null | cut -f 1 -d ':')" -- "${cur}") )
 }
-function _tmux_complete_window() {
+function _tmux_complete_window {
   local IFS=$'\n'
-  local cur="${1}"
-  local session_name="$(echo "${cur}" | sed 's/\\//g' | cut -d ':' -f 1)"
+  local cur=$1
+  local session_name=$(<<< "$cur" sed 's/\\//g' | cut -d ':' -f 1)
   local sessions
 
   sessions="$(tmux -q list-sessions 2>/dev/null | sed -re 's/([^:]+:).*$/\1/')"
-  if [[ -n "${session_name}" ]]; then
-    sessions="${sessions}
-        $(tmux -q list-windows -t "${session_name}" 2>/dev/null | sed -re 's/^([^:]+):.*$/'"${session_name}"':\1/')"
+  if [[ $session_name ]]; then
+    sessions="$sessions
+        $(tmux -q list-windows -t "$session_name" 2>/dev/null | sed -re 's/^([^:]+):.*$/'"$session_name"':\1/')"
   fi
-  cur="$(echo "${cur}" | sed -e 's/:/\\\\:/')"
-  sessions="$(echo "${sessions}" | sed -e 's/:/\\\\:/')"
-  COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "${sessions}" -- "${cur}") )
+  cur=$(<<< "$cur" sed -e 's/:/\\\\:/')
+  sessions=$(<<< "$sessions" sed -e 's/:/\\\\:/')
+  COMPREPLY=("${COMPREPLY[@]}" $(compgen -W "$sessions" -- "$cur"))
 }
 
 function _tmux {
@@ -62,21 +57,21 @@ function _tmux {
   local i cmd cmd_index option option_index
   local opts=""
   COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
 
-  if [ ${prev} == -f ]; then
+  if [[ $prev == -f ]]; then
     _tmux_filedir
   else
     # Search for the command
     local skip_next=0
-    for ((i=1; $i<=$COMP_CWORD; i++)); do
-      if [[ ${skip_next} -eq 1 ]]; then
+    for ((i = 1; i <= COMP_CWORD; i++)); do
+      if ((skip_next == 1)) then
         #echo "Skipping"
         skip_next=0;
       elif [[ ${COMP_WORDS[i]} != -* ]]; then
-        cmd="${COMP_WORDS[i]}"
-        cmd_index=${i}
+        cmd=${COMP_WORDS[i]}
+        cmd_index=$i
         break
       elif [[ ${COMP_WORDS[i]} == -f ]]; then
         skip_next=1
@@ -85,49 +80,49 @@ function _tmux {
 
     # Search for the last option command
     skip_next=0
-    for ((i=1; $i<=$COMP_CWORD; i++)); do
-      if [[ ${skip_next} -eq 1 ]]; then
+    for ((i = 1; i <= COMP_CWORD; i++)); do
+      if ((skip_next == 1)); then
         #echo "Skipping"
-        skip_next=0;
+        skip_next=0
       elif [[ ${COMP_WORDS[i]} == -* ]]; then
-        option="${COMP_WORDS[i]}"
-        option_index=${i}
+        option=${COMP_WORDS[i]}
+        option_index=$i
         if [[ ${COMP_WORDS[i]} == -- ]]; then
-          break;
+          break
         fi
       elif [[ ${COMP_WORDS[i]} == -f ]]; then
         skip_next=1
       fi
     done
 
-    if [[ $COMP_CWORD -le $cmd_index ]]; then
+    if ((COMP_CWORD <= cmd_index)); then
       # The user has not specified a command yet
-      COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "$(tmux start-server \; list-commands | cut -d' ' -f1)" -- "${cur}") )
+      COMPREPLY=("${COMPREPLY[@]}" $(compgen -W "$(tmux start-server \; list-commands | cut -d' ' -f1)" -- "$cur"))
     else
-      case ${cmd} in
+      case $cmd in
       attach-session|attach)
-        case "$prev" in
-        -t) _tmux_complete_session "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_session "$cur" ;;
         *) options="-t -d" ;;
         esac ;;
       detach-client|detach)
-        case "$prev" in
-        -t) _tmux_complete_client "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_client "$cur" ;;
         *) options="-t" ;;
         esac ;;
       lock-client|lockc)
-        case "$prev" in
-        -t) _tmux_complete_client "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_client "$cur" ;;
         *) options="-t" ;;
         esac ;;
       lock-session|locks)
-        case "$prev" in
-        -t) _tmux_complete_session "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_session "$cur" ;;
         *) options="-t -d" ;;
         esac ;;
       new-session|new)
-        case "$prev" in
-        -t) _tmux_complete_session "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_session "$cur" ;;
         -[n|d|s]) options="-d -n -s -t --" ;;
         *)
           if [[ ${COMP_WORDS[option_index]} == -- ]]; then
@@ -139,44 +134,44 @@ function _tmux {
         esac
         ;;
       refresh-client|refresh)
-        case "$prev" in
-        -t) _tmux_complete_client "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_client "$cur" ;;
         *) options="-t" ;;
         esac ;;
       rename-session|rename)
-        case "$prev" in
-        -t) _tmux_complete_session "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_session "$cur" ;;
         *) options="-t" ;;
         esac ;;
       source-file|source) _tmux_filedir ;;
       has-session|has|kill-session)
-        case "$prev" in
-        -t) _tmux_complete_session "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_session "$cur" ;;
         *) options="-t" ;;
         esac ;;
       suspend-client|suspendc)
-        case "$prev" in
-        -t) _tmux_complete_client "${cur}" ;;
+        case $prev in
+        -t) _tmux_complete_client "$cur" ;;
         *) options="-t" ;;
         esac ;;
       switch-client|switchc)
-        case "$prev" in
-        -c) _tmux_complete_client "${cur}" ;;
-        -t) _tmux_complete_session "${cur}" ;;
+        case $prev in
+        -c) _tmux_complete_client "$cur" ;;
+        -t) _tmux_complete_session "$cur" ;;
         *) options="-l -n -p -c -t" ;;
         esac ;;
 
       send-keys|send)
-        case "$option" in
-        -t) _tmux_complete_window "${cur}" ;;
+        case $option in
+        -t) _tmux_complete_window "$cur" ;;
         *) options="-t" ;;
         esac ;;
-      esac # case ${cmd}
+      esac # case $cmd
     fi # command specified
   fi # not -f
 
-  if [[ -n "${options}" ]]; then
-    COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "${options}" -- "${cur}") )
+  if [[ $options ]]; then
+    COMPREPLY=("${COMPREPLY[@]}" $(compgen -W "$options" -- "$cur"))
   fi
 
   return 0
@@ -184,4 +179,3 @@ function _tmux {
 complete -F _tmux tmux
 
 # END tmux completion
-

completions/defaults.completion.sh
@@ -1,4 +1,17 @@
 #! bash oh-my-bash.module
+#
+# Note: I could not find the true original source of this completion, but we
+# can find several versions at the following places:
+#
+# * https://gist.github.com/mbauman/839902
+# * https://github.com/taylanpince/wiki/blob/master/scripts/bash_completion_scripts/defaults (Version 1.0)
+# * https://github.com/gaelicWizard/bash-progcomp/tree/main (Version 2.0)
+#
+# The v2 script seems to be a complete rewrite of the original v1.0, though it
+# retains the original copyright notice.  The current version is based on
+# Version 1.0.
+#
+#------------------------------------------------------------------------------
 # defaults
 # Bash command line completion for defaults
 #
@@ -7,25 +20,20 @@
 #
 # Version 1.0 (2006-11-08)
 
-_defaults_domains()
-{
+function _defaults_domains {
   local cur
   COMPREPLY=()
   cur=${COMP_WORDS[COMP_CWORD]}
 
-  local domains=$( defaults domains | sed -e 's/, /:/g' | tr : '\n' | sed -e 's/ /\\ /g' | grep -i "^$cur" )
-  local IFS=$'\n'
-  COMPREPLY=( $domains )
-  if [[ $( echo '-app' | grep "^$cur" ) ]]; then
+  local domains=$(defaults domains | sed -e 's/, /:/g' | tr : '\n' | sed -e 's/ /\\ /g' | grep -i "^$cur")
+  _omb_util_split COMPREPLY "$domains" $'\n'
+  if grep -q "^$cur" <<< '-app'; then
     COMPREPLY[${#COMPREPLY[@]}]="-app"
   fi
-
   return 0
 }
 
-
-_defaults()
-{
+function _defaults {
   local cur prev host_opts cmds cmd domain keys key_index
   cur=${COMP_WORDS[COMP_CWORD]}
   prev=${COMP_WORDS[COMP_CWORD-1]}
@@ -33,21 +41,21 @@ _defaults()
   host_opts='-currentHost -host'
   cmds='read read-type write rename delete domains find help'
 
-  if [[ $COMP_CWORD -eq 1 ]]; then
-    COMPREPLY=( $( compgen -W "$host_opts $cmds" -- $cur ) )
+  if ((COMP_CWORD == 1)); then
+    COMPREPLY=( $(compgen -W "$host_opts $cmds" -- "$cur") )
     return 0
-  elif [[ $COMP_CWORD -eq 2 ]]; then
-    if [[ "$prev" == "-currentHost" ]]; then
-      COMPREPLY=( $( compgen -W "$cmds" -- $cur ) )
+  elif ((COMP_CWORD == 2)); then
+    if [[ $prev == "-currentHost" ]]; then
+      COMPREPLY=( $(compgen -W "$cmds" -- "$cur") )
       return 0
-    elif [[ "$prev" == "-host" ]]; then
+    elif [[ $prev == "-host" ]]; then
       return 0
       _known_hosts -a
     else
       _defaults_domains
       return 0
     fi
-  elif [[ $COMP_CWORD -eq 3 ]]; then
+  elif ((COMP_CWORD == 3)); then
     if [[ ${COMP_WORDS[1]} == "-host" ]]; then
       _defaults_domains
       return 0
@@ -60,15 +68,15 @@ _defaults()
     cmd=${COMP_WORDS[1]}
     domain=${COMP_WORDS[2]}
     key_index=3
-    if [[ "$domain" == "-app" ]]; then
-      if [[ $COMP_CWORD -eq 3 ]]; then
+    if [[ $domain == "-app" ]]; then
+      if ((COMP_CWORD == 3)); then
         # Completing application name. Can't help here, sorry
         return 0
       fi
       domain="-app ${COMP_WORDS[3]}"
       key_index=4
     fi
-  elif [[ ${COMP_WORDS[2]} == "-currentHost" ]] && [[ ${COMP_WORDS[2]} == [${cmds// /|}] ]]; then
+  elif [[ ${COMP_WORDS[2]} == "-currentHost" && ${COMP_WORDS[2]} == [${cmds// /|}] ]]; then
     cmd=${COMP_WORDS[2]}
     domain=${COMP_WORDS[3]}
     key_index=4
@@ -80,12 +88,12 @@ _defaults()
       domain="-app ${COMP_WORDS[4]}"
       key_index=5
     fi
-  elif [[ ${COMP_WORDS[3]} == "-host" ]] && [[ ${COMP_WORDS[3]} == [${cmds// /|}] ]]; then
+  elif [[ ${COMP_WORDS[3]} == "-host" && ${COMP_WORDS[3]} == [${cmds// /|}] ]]; then
     cmd=${COMP_WORDS[3]}
     domain=${COMP_WORDS[4]}
     key_index=5
-    if [[ "$domain" == "-app" ]]; then
-      if [[ $COMP_CWORD -eq 5 ]]; then
+    if [[ $domain == "-app" ]]; then
+      if ((COMP_CWORD == 5)); then
         # Completing application name. Can't help here, sorry
         return 0
       fi
@@ -94,48 +102,50 @@ _defaults()
     fi
   fi
 
-  keys=$( defaults read $domain 2>/dev/null | sed -n -e '/^    [^}) ]/p' | sed -e 's/^    \([^" ]\{1,\}\) = .*$/\1/g' -e 's/^    "\([^"]\{1,\}\)" = .*$/\1/g' | sed -e 's/ /\\ /g' )
+  keys=$(defaults read $domain 2>/dev/null |
+           sed -ne '/^    [^}) ]/p' |
+           sed -e 's/^    \([^" ]\{1,\}\) = .*$/\1/g' -e 's/^    "\([^"]\{1,\}\)" = .*$/\1/g' |
+           sed -e 's/ /\\ /g' )
 
   case $cmd in
   read|read-type)
     # Complete key
     local IFS=$'\n'
-    COMPREPLY=( $( echo "$keys" | grep -i "^${cur//\\/\\\\}" ) )
+    COMPREPLY=( $(grep -i "^${cur//\\/\\\\}" <<< "$keys") )
     ;;
   write)
-    if [[ $key_index -eq $COMP_CWORD ]]; then
+    if ((key_index == COMP_CWORD)); then
       # Complete key
       local IFS=$'\n'
-      COMPREPLY=( $( echo "$keys" | grep -i "^${cur//\\/\\\\}" ) )
-    elif [[ $((key_index+1)) -eq $COMP_CWORD ]]; then
+      COMPREPLY=( $(grep -i "^${cur//\\/\\\\}" <<< "$keys") )
+    elif ((key_index + 1 == COMP_CWORD)); then
       # Complete value type
       # Unfortunately ${COMP_WORDS[key_index]} fails on keys with spaces
       local value_types='-string -data -integer -float -boolean -date -array -array-add -dict -dict-add'
-      local cur_type=$( defaults read-type $domain ${COMP_WORDS[key_index]} 2>/dev/null | sed -e 's/^Type is \(.*\)/-\1/' -e's/dictionary/dict/' | grep "^$cur" )
+      local cur_type=$(defaults read-type $domain ${COMP_WORDS[key_index]} 2>/dev/null | sed -e 's/^Type is \(.*\)/-\1/' -e's/dictionary/dict/' | grep "^$cur")
       if [[ $cur_type ]]; then
         COMPREPLY=( $cur_type )
       else
-        COMPREPLY=( $( compgen -W "$value_types" -- $cur ) )
+        COMPREPLY=( $(compgen -W "$value_types" -- "$cur") )
       fi
-    elif [[ $((key_index+2)) -eq $COMP_CWORD ]]; then
+    elif ((key_index + 2 == COMP_CWORD)); then
       # Complete value
       # Unfortunately ${COMP_WORDS[key_index]} fails on keys with spaces
-      COMPREPLY=( $( defaults read $domain ${COMP_WORDS[key_index]} 2>/dev/null | grep -i "^${cur//\\/\\\\}" ) )
+      COMPREPLY=( $(defaults read $domain ${COMP_WORDS[key_index]} 2>/dev/null | grep -i "^${cur//\\/\\\\}") )
     fi
     ;;
   rename)
-    if [[ $key_index -eq $COMP_CWORD ]] ||
-         [[ $((key_index+1)) -eq $COMP_CWORD ]]; then
+    if ((key_index == COMP_CWORD || key_index + 1 == COMP_CWORD)); then
       # Complete source and destination keys
       local IFS=$'\n'
-      COMPREPLY=( $( echo "$keys" | grep -i "^${cur//\\/\\\\}" ) )
+      COMPREPLY=( $(grep -i "^${cur//\\/\\\\}" <<< "$keys") )
     fi
     ;;
   delete)
-    if [[ $key_index -eq $COMP_CWORD ]]; then
+    if ((key_index == COMP_CWORD)); then
       # Complete key
       local IFS=$'\n'
-      COMPREPLY=( $( echo "$keys" | grep -i "^${cur//\\/\\\\}" ) )
+      COMPREPLY=( $(grep -i "^${cur//\\/\\\\}" <<< "$keys") )
     fi
     ;;
   esac

completions/go.completion.sh
@@ -1,8 +1,8 @@
 #! bash oh-my-bash.module
-
+#
 # bash completion for go tool
-# https://github.com/thomasf/go-bash-completion
-
+# https://github.com/thomasf/go-bash-completion/blob/73c7543f2a295d27ae9bc71baa1ef1a858447eae/go-bash-completion.bash
+#------------------------------------------------------------------------------
 # install in /etc/bash_completion.d/ or your personal directory
 
 complete -f -X '!*.8' 8l
@@ -11,29 +11,27 @@ complete -f -X '!*.5' 5l
 complete -f -X '!*.go' 8g 6g 5g gofmt gccgo
 
 function _go_clear_cache {
-  unset _go_imports
+  unset -v _go_imports
 }
 function _go_importpath_cache {
-  if [ -z "$_go_imports" ]; then
+  if [[ ! ${_go_imports-} ]]; then
     _go_imports=$(go list all 2>/dev/null)
     export _go_imports
   fi
 }
 
-_go_importpath()
-{
-  echo "$(compgen -W "$_go_imports" -- "$1")"
+function _go_importpath {
+  compgen -W "$_go_imports" -- "$1"
 }
 
-_go()
-{
+function _go {
   # TODO: Only allow flags before other arguments. run already does
   # this.
 
-  local cur=`_get_cword`
-  local prev="${COMP_WORDS[COMP_CWORD-1]}"
+  local cur=$(_get_cword)
+  local prev=${COMP_WORDS[COMP_CWORD-1]}
 
-  local cmd="${COMP_WORDS[1]}"
+  local cmd=${COMP_WORDS[1]}
 
   local cmds="build clean doc env fix fmt get
     install list run test tool version vet"
@@ -43,37 +41,38 @@ _go()
   local env_vars="GOARCH GOBIN GOEXE GOHOSTARCH GOHOSTOS GOOS GOPATH GORACE
     GOROOT GOTOOLDIR GO15VENDOREXPERIMENT CC GOGCCFLAGS CXX CGO_ENABLED"
 
-  if [ "$COMP_CWORD" == 1 ]; then
+  if ((COMP_CWORD == 1)); then
+    local opt
     for opt in $cmds; do
-      if [[ "$opt" == "$cmd" ]]; then
+      if [[ $opt == "$cmd" ]]; then
         COMPREPLY=("$opt")
-        return
+        return 0
       fi
     done
   fi
 
-  case "$cmd" in
+  case $cmd in
   'build')
-    case "$prev" in
+    case $prev in
     '-o')
       _filedir
       ;;
     '-p')
       ;;
     *)
-      if [[ "$cur" == -* ]]; then
+      if [[ $cur == -* ]]; then
         COMPREPLY=($(compgen -W "-a -n -o -p -v -x" -- "$cur"))
       else
-        local found=0
+        local found=0 i
         for ((i=0; i < ${#COMP_WORDS[@]}; i++)); do
-          case "$i" in
+          case $i in
           0|1|"$COMP_CWORD")
             continue
             ;;
           esac
-          local opt="${COMP_WORDS[i]}"
-          if [[ "$opt" != -* ]]; then
-            if [[ "$opt" == *.go && -f "$opt" ]]; then
+          local opt=${COMP_WORDS[i]}
+          if [[ $opt != -* ]]; then
+            if [[ $opt == *.go && -f $opt ]]; then
               found=1
               break
             else
@@ -82,18 +81,18 @@ _go()
             fi
           fi
         done
-        case "$found" in
+        case $found in
         0)
           _filedir go
           _go_importpath_cache
-          COMPREPLY+=(`_go_importpath "$cur"`)
+          COMPREPLY+=($(_go_importpath "$cur"))
           ;;
         1)
           _filedir go
           ;;
         2)
           _go_importpath_cache
-          COMPREPLY=(`_go_importpath "$cur"`)
+          COMPREPLY=($(_go_importpath "$cur"))
           ;;
         esac
       fi
@@ -101,99 +100,99 @@ _go()
     esac
     ;;
   'clean')
-    if [[ "$cur" == -* ]]; then
+    if [[ $cur == -* ]]; then
       COMPREPLY=($(compgen -W "-i -r -n -x" -- "$cur"))
     else
       _go_importpath_cache
-      COMPREPLY=(`_go_importpath "$cur"`)
+      COMPREPLY=($(_go_importpath "$cur"))
     fi
     ;;
   'doc')
     _go_importpath_cache
-    COMPREPLY=(`_go_importpath "$cur"`)
+    COMPREPLY=($(_go_importpath "$cur"))
     ;;
   'env')
     COMPREPLY=($(compgen -W "$env_vars" -- "$cur"))
     ;;
   'fix')
     _go_importpath_cache
-    COMPREPLY=(`_go_importpath "$cur"`)
+    COMPREPLY=($(_go_importpath "$cur"))
     ;;
   'fmt')
     _go_importpath_cache
-    COMPREPLY=(`_go_importpath "$cur"`)
+    COMPREPLY=($(_go_importpath "$cur"))
     ;;
   'get')
-    case "$prev" in
+    case $prev in
     '-p')
     ;;
     *)
-      if [[ "$cur" == -* ]]; then
+      if [[ $cur == -* ]]; then
         COMPREPLY=($(compgen -W "-a -d -fix -n -p -u -v -x" -- "$cur"))
       else
         _go_importpath_cache
-        COMPREPLY=(`_go_importpath "$cur"`)
+        COMPREPLY=($(_go_importpath "$cur"))
       fi
       ;;
     esac
     ;;
   'install')
-    case "$prev" in
+    case $prev in
     '-p')
     ;;
     *)
-      if [[ "$cur" == -* ]]; then
+      if [[ $cur == -* ]]; then
         COMPREPLY=($(compgen -W "-a -n -p -v -x" -- "$cur"))
       else
         _go_importpath_cache
-        COMPREPLY=(`_go_importpath "$cur"`)
+        COMPREPLY=($(_go_importpath "$cur"))
       fi
       ;;
     esac
     ;;
   'list')
-    case "$prev" in
+    case $prev in
     '-f')
     ;;
     *)
-      if [[ "$cur" == -* ]]; then
+      if [[ $cur == -* ]]; then
         COMPREPLY=($(compgen -W "-e -f -json" -- "$cur"))
       else
         _go_importpath_cache
-        COMPREPLY=(`_go_importpath "$cur"`)
+        COMPREPLY=($(_go_importpath "$cur"))
       fi
       ;;
     esac
     ;;
   'run')
-    if [[ "$cur" == -* && "$prev" != *.go ]]; then
+    if [[ $cur == -* && $prev != *.go ]]; then
       COMPREPLY=($(compgen -W "-a -n -x" -- "$cur"))
     else
       _filedir
     fi
     ;;
   'test') # TODO: Support for testflags.
-    case "$prev" in
+    case $prev in
     '-file')
       _filedir go
       ;;
     '-p')
       ;;
     *)
-      if [[ "$cur" == -* ]]; then
+      if [[ $cur == -* ]]; then
         COMPREPLY=($(compgen -W "-c -file -i -p -x" -- "$cur"))
       else
         _go_importpath_cache
-        COMPREPLY=(`_go_importpath "$cur"`)
+        COMPREPLY=($(_go_importpath "$cur"))
       fi
       ;;
     esac
     ;;
   'tool')
-    if [ "$COMP_CWORD" == 2 ]; then
+    if ((COMP_CWORD == 2)); then
       COMPREPLY=($(compgen -W "$(go tool)" -- "$cur"))
     else
-      case "${COMP_WORDS[2]}" in
+      case ${COMP_WORDS[2]} in
       [568]a) # TODO: Implement something.
       #_go_tool_568a
       ;;
@@ -246,7 +245,7 @@ _go()
       #_go_tool_yacc
       ;;
       esac
-      if [[ "$cur" == -* ]]; then
+      if [[ $cur == -* ]]; then
         COMPREPLY=($(compgen -W "${COMPREPLY[*]} -h" -- "$cur"))
       fi
     fi
@@ -254,20 +253,20 @@ _go()
   'version')
     ;;
   'vet')
-    if [[ "$cur" == -* ]]; then
+    if [[ $cur == -* ]]; then
       :
     else
       _go_importpath_cache
-      COMPREPLY=(`_go_importpath "$cur"`)
+      COMPREPLY=($(_go_importpath "$cur"))
     fi
     ;;
   'help')
-    if [ "$COMP_CWORD" == 2 ]; then
+    if ((COMP_CWORD == 2)); then
       COMPREPLY=($(compgen -W "$cmds $addhelp" -- "$cur"))
     fi
     ;;
   *)
-    if [ "$COMP_CWORD" == 1 ]; then
+    if ((COMP_CWORD == 1)); then
       COMPREPLY=($(compgen -W "$cmds $other" -- "$cur"))
     else
       _filedir

completions/gradle.completion.sh
@@ -1,8 +1,10 @@
 #! bash oh-my-bash.module
+# This completion setting seems to be only found in Oh My Bash.
+#------------------------------------------------------------------------------
 function __gradle {
   local cur=${COMP_WORDS[COMP_CWORD]}
   local tasks=''
-  local cache_dir="$HOME/.gradle/completion_cache"
+  local cache_dir=$HOME/.gradle/completion_cache
 
   case $OSTYPE in
   darwin*)
@@ -14,34 +16,34 @@ function __gradle {
   esac
   local parsing_command="gradle --console=plain --quiet tasks | grep -v Rules | sed -nE -e 's/^([a-zA-Z]+)($| - .+)/\1/p'"
 
-  mkdir -p "${cache_dir}"
+  mkdir -p "$cache_dir"
 
   local gradle_files_checksum='no_cache_file'
   if [[ -f build.gradle ]]; then
-    gradle_files_checksum="$(eval "${checksum_command}")"
-    if [[ -f "${cache_dir}/${gradle_files_checksum}" ]]; then
-      newest_gradle_file="$(find . -type f -name build.gradle -newer "${cache_dir}/${gradle_files_checksum}")"
-      if [ -n "${newest_gradle_file}" ]; then
-        tasks="$(eval "${parsing_command}")"
-        [[ -n "${tasks}" ]] && echo "${tasks}" > "${cache_dir}/${gradle_files_checksum}"
+    gradle_files_checksum=$(eval "$checksum_command")
+    if [[ -f $cache_dir/$gradle_files_checksum ]]; then
+      newest_gradle_file=$(find . -type f -name build.gradle -newer "$cache_dir/$gradle_files_checksum")
+      if [[ $newest_gradle_file ]]; then
+        tasks=$(eval "$parsing_command")
+        [[ $tasks ]] && echo "$tasks" > "$cache_dir/$gradle_files_checksum"
       else
-        tasks="$(cat "${cache_dir}/${gradle_files_checksum}")"
-        touch "${cache_dir}/${gradle_files_checksum}"
+        tasks=$(< "$cache_dir/$gradle_files_checksum")
+        touch "$cache_dir/$gradle_files_checksum"
       fi
     else
-      tasks="$(eval "${parsing_command}")"
-      [[ -n "${tasks}" ]] && echo "${tasks}" > "${cache_dir}/${gradle_files_checksum}"
+      tasks=$(eval "$parsing_command")
+      [[ $tasks ]] && echo "$tasks" > "$cache_dir/$gradle_files_checksum"
     fi
   else
-    tasks="$(eval "${parsing_command}")"
-    [[ -n "${tasks}" ]] && echo "${tasks}" > "${cache_dir}/${gradle_files_checksum}"
+    tasks=$(eval "$parsing_command")
+    [[ $tasks ]] && echo "$tasks" > "$cache_dir/$gradle_files_checksum"
   fi
-  COMPREPLY=( $(compgen -W "${tasks}" -- "${cur}") )
+  COMPREPLY=($(compgen -W "$tasks" -- "$cur"))
 }
 
 function __clear_gradle_cache {
-  local cache_dir="$HOME/.gradle/completion_cache"
-  [[ -d "${cache_dir}" ]] && find "${cache_dir}" -type f -mtime +7 -exec rm -f {} \;
+  local cache_dir=$HOME/.gradle/completion_cache
+  [[ -d $cache_dir ]] && find "$cache_dir" -type f -mtime +7 -exec rm -f {} \;
 }
 
 __clear_gradle_cache

completions/grunt.completion.sh
@@ -1,5 +1,12 @@
 #! bash oh-my-bash.module
-
+#
+# The current version is based on the following upstream version:
+# https://github.com/gruntjs/grunt-cli/blob/8c791efc931fa8cf80cc98d09d3e20c36501fc0f/completion/bash#L33
+#
+# Note: The upstream version seems to have just updated the copyright year, but
+# that change must be wrong.  Basically, the copyright year should reflect the
+# year of the first release.  The years of later updates are optional.
+#------------------------------------------------------------------------------
 # grunt-cli
 # http://gruntjs.com/
 #
@@ -15,33 +22,34 @@
 # eval "$(grunt --completion=bash)"
 
 # Search the current directory and all parent directories for a gruntfile.
-function _grunt_gruntfile() {
-  local curpath="$PWD"
-  while [[ "$curpath" ]]; do
-    for gruntfile in "$curpath/"{G,g}runtfile.{js,coffee}; do
-      if [[ -e "$gruntfile" ]]; then
+function _grunt_gruntfile {
+  local curpath=$PWD
+  while [[ $curpath ]]; do
+    local gruntfile
+    for gruntfile in "$curpath"/{G,g}runtfile.{js,coffee}; do
+      if [[ -e $gruntfile ]]; then
         echo "$gruntfile"
-        return
+        return 0
       fi
     done
-    curpath="${curpath%/*}"
+    curpath=${curpath%/*}
   done
   return 1
 }
 
 # Enable bash autocompletion.
-function _grunt_completions() {
+function _grunt_completions {
   # The currently-being-completed word.
-  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local cur=${COMP_WORDS[COMP_CWORD]}
   # The current gruntfile, if it exists.
-  local gruntfile="$(_grunt_gruntfile)"
+  local gruntfile=$(_grunt_gruntfile)
   # The current grunt version, available tasks, options, etc.
-  local gruntinfo="$(grunt --version --verbose 2>/dev/null)"
+  local gruntinfo=$(grunt --version --verbose 2>/dev/null)
   # Options and tasks.
-  local opts="$(echo "$gruntinfo" | awk '/Available options: / {$1=$2=""; print $0}')"
-  local compls="$(echo "$gruntinfo" | awk '/Available tasks: / {$1=$2=""; print $0}')"
+  local opts=$(awk '/Available options: / {$1=$2=""; print $0}' <<< "$gruntinfo")
+  local compls=$(awk '/Available tasks: / {$1=$2=""; print $0}' <<< "$gruntinfo")
   # Only add -- or - options if the user has started typing -
-  [[ "$cur" == -* ]] && compls="$compls $opts"
+  [[ $cur == -* ]] && compls="$compls $opts"
   # Tell complete what stuff to show.
   COMPREPLY=($(compgen -W "$compls" -- "$cur"))
 }

commit a3ea82a5be4b235f202704cafb9aaad43de1972c

fix(completions/tmux): fix syntax
Regression introduced by commit 52987b3d.  This is a typo.  The reason
that it slipped off checking was because the syntax "if (()) then"
became valid in Bash 5.2, and it was tested using Bash 5.3-alpha.

completions/tmux.completion.sh
@@ -66,7 +66,7 @@ function _tmux {
     # Search for the command
     local skip_next=0
     for ((i = 1; i <= COMP_CWORD; i++)); do
-      if ((skip_next == 1)) then
+      if ((skip_next == 1)); then
         #echo "Skipping"
         skip_next=0;
       elif [[ ${COMP_WORDS[i]} != -* ]]; then