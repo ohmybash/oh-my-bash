db0caf14d87a53b7a61e072f953968ef2fdfd2b4
clebron949 <christopher.lebron1@gmail.com>
2024-08-31 08:44:05 -0400

    Add plugin for dotnet CLI

---
+++ plugins/dotnet/dotnet.plugin.sh
@@ -0,0 +1,34 @@
+# This scripts is copied from (MIT License):
+
+# Create a new .NET project
+alias dn='dotnet new'
+
+# Build and run a .NET project
+alias dr='dotnet run'
+
+# Test a .NET project
+alias dt='dotnet test'
+
+# Run a .NET project in watch mode
+alias dw='dotnet watch'
+
+#
+alias dwr='dotnet watch run'
+
+#
+alias dwt='dotnet watch test'
+
+#
+alias ds='dotnet sln'
+
+#
+alias da='dotnet add'
+
+#
+alias dp='dotnet pack'
+
+#
+alias dng='dotnet nuget'
+
+#
+alias db='dotnet build'
