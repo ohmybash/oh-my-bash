28f2fadcb45b24aeabc9efd6968d1cb231bd91bf
clebron949 <christopher.lebron1@gmail.com>
2024-08-31 09:03:58 -0400

    Refactor add more comments to help with description

--- plugins/dotnet/dotnet.plugin.sh
+++ plugins/dotnet/dotnet.plugin.sh
@@ -1,34 +1,40 @@
 # This scripts is copied from (MIT License):
 
-# Create a new .NET project
+#Create a new .NET project or file.
 alias dn='dotnet new'
 
-# Build and run a .NET project
+#Build and run a .NET project output.
 alias dr='dotnet run'
 
-# Test a .NET project
+#Run unit tests using the test runner specified in a .NET project.
 alias dt='dotnet test'
 
-# Run a .NET project in watch mode
+#Watch for source file changes and restart the dotnet command.
 alias dw='dotnet watch'
 
-#
+#Watch for source file changes and restart the `run` command.
 alias dwr='dotnet watch run'
 
-#
+#Watch for source file changes and restart the `test` command.
 alias dwt='dotnet watch test'
 
-#
+#Modify Visual Studio solution files.
 alias ds='dotnet sln'
 
-#
+#Add a package or reference to a .NET project.
 alias da='dotnet add'
 
-#
+#Create a NuGet package.
 alias dp='dotnet pack'
 
-#
+#Provides additional NuGet commands.
 alias dng='dotnet nuget'
 
-#
+#Build a .NET project
 alias db='dotnet build'
+
+#List dotnet sdk versions installed
+alias dls='dotnet --list-sdks'
+
+#List dotnet runtimes installed
+alias dlr='dotnet --list-runtimes'
