0d9c63479c9a9ae18289eff5e7ac08a4ce0ae46d
clebron949 <christopher.lebron1@gmail.com>
2024-09-01 08:14:20 -0400

    Update: Add dotnet completion source.

--- plugins/dotnet/dotnet.plugin.sh
+++ plugins/dotnet/dotnet.plugin.sh
@@ -3,6 +3,22 @@
 # This scripts is copied from (MIT License):
 # https://raw.githubusercontent.com/dotnet/sdk/main/scripts/register-completions.zsh
 
+# bash parameter completion for the dotnet CLI
+# source: https://raw.githubusercontent.com/dotnet/sdk/main/scripts/register-completions.bash
+_dotnet_bash_complete()
+{
+  local word=${COMP_WORDS[COMP_CWORD]}
+
+  local completions
+  completions="$(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)"
+  if [ $? -ne 0 ]; then
+    completions=""
+  fi
+
+  COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+}
+complete -f -F _dotnet_bash_complete dotnet
+
 
 # Aliases bellow are here for backwards compatibility
 # added by Shaun Tabone (https://github.com/xontab)
