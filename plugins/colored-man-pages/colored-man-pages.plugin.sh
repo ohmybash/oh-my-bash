89c86b0b4f7b373611603322638fb8489bbcb1dd
Koichi Murase <myoga.murase@gmail.com>
2024-10-02 13:24:28 +0900

    style(plugins/colored-man-pages): adjust styles

--- plugins/colored-man-pages/colored-man-pages.plugin.sh
+++ plugins/colored-man-pages/colored-man-pages.plugin.sh
@@ -6,14 +6,14 @@
 # https://github.com/ohmyzsh/ohmyzsh/blob/6bc4c80c7db072a0d2d265eb3589bbe52e0d2737/plugins/colored-man-pages/colored-man-pages.plugin.zsh
 
 # Adds the LESS_TERMCAP_* variables to color the man pages.
-function colored() {
-  local -x LESS_TERMCAP_mb=${_omb_term_red}
-  local -x LESS_TERMCAP_md=${_omb_term_bold_red}
-  local -x LESS_TERMCAP_me=${_omb_term_reset}
-  local -x LESS_TERMCAP_so=${_omb_term_bold_yellow}
-  local -x LESS_TERMCAP_se=${_omb_term_reset}
-  local -x LESS_TERMCAP_us=${_omb_term_green}
-  local -x LESS_TERMCAP_ue=${_omb_term_reset}
+function colored {
+  local -x LESS_TERMCAP_mb=$_omb_term_red
+  local -x LESS_TERMCAP_md=$_omb_term_bold_red
+  local -x LESS_TERMCAP_me=$_omb_term_reset
+  local -x LESS_TERMCAP_so=$_omb_term_bold_yellow
+  local -x LESS_TERMCAP_se=$_omb_term_reset
+  local -x LESS_TERMCAP_us=$_omb_term_green
+  local -x LESS_TERMCAP_ue=$_omb_term_reset
 
   # Prefer `less` whenever available, since we specifically configured
   # environment for it.
@@ -25,18 +25,18 @@ function colored() {
 
 # Wrapper for man to colorize the output.
 _omb_util_binary_exists man &&
-function man {
-  colored "$FUNCNAME" "$@"
-}
+  function man {
+    colored "$FUNCNAME" "$@"
+  }
 
 # Wrapper for dman to colorize the output.
 _omb_util_binary_exists dman &&
-function dman {
-  colored "$FUNCNAME" "$@"
-}
+  function dman {
+    colored "$FUNCNAME" "$@"
+  }
 
 # Wrapper for debman to colorize the output.
 _omb_util_binary_exists debman &&
-function debman {
-  colored "$FUNCNAME" "$@"
-}
+  function debman {
+    colored "$FUNCNAME" "$@"
+  }
