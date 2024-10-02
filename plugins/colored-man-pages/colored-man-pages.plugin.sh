497f628274082f178babcafd00e3a0fe00e63e2d
Rob Loach <robloach@gmail.com>
2024-10-01 22:48:06 -0400

    colored_man_pages: Fix enviroment variable

--- plugins/colored-man-pages/colored-man-pages.plugin.sh
+++ plugins/colored-man-pages/colored-man-pages.plugin.sh
@@ -5,24 +5,22 @@
 # This plugin is based on the following Oh-My-Zsh plugin:
 # https://github.com/ohmyzsh/ohmyzsh/blob/6bc4c80c7db072a0d2d265eb3589bbe52e0d2737/plugins/colored-man-pages/colored-man-pages.plugin.zsh
 
-# Adds the LESS_TERMCAP_* variables to the environment to color man pages.
+# Adds the LESS_TERMCAP_* variables to color the man pages.
 function colored() {
-  local -a environment
-
-  environment+=( LESS_TERMCAP_mb=${_omb_term_red} )
-  environment+=( LESS_TERMCAP_md=${_omb_term_bold_red} )
-  environment+=( LESS_TERMCAP_me=${_omb_term_reset} )
-  environment+=( LESS_TERMCAP_so=${_omb_term_bold_yellow} )
-  environment+=( LESS_TERMCAP_se=${_omb_term_reset} )
-  environment+=( LESS_TERMCAP_us=${_omb_term_green} )
-  environment+=( LESS_TERMCAP_ue=${_omb_term_reset} )
+  local -x LESS_TERMCAP_mb=${_omb_term_green}
+  local -x LESS_TERMCAP_md=${_omb_term_bold_green}
+  local -x LESS_TERMCAP_me=${_omb_term_reset}
+  local -x LESS_TERMCAP_so=${_omb_term_bold_yellow}
+  local -x LESS_TERMCAP_se=${_omb_term_reset}
+  local -x LESS_TERMCAP_us=${_omb_term_purple}
+  local -x LESS_TERMCAP_ue=${_omb_term_reset}
 
   # Prefer `less` whenever available, since we specifically configured
   # environment for it.
-  environment+=( PAGER="${commands[less]:-$PAGER}" )
-  environment+=( GROFF_NO_SGR=1 )
+  local -x PAGER="${commands[less]:-$PAGER}"
+  local -x GROFF_NO_SGR=1
 
-  env "${environment[@]}" "$@"
+  command "$@"
 }
 
 # Wrapper for man to colorize the output.
