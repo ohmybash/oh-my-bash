cf370ce90ac9f60fd5a348a02d9cb130924966b9
Koichi Murase <myoga.murase@gmail.com>
2024-11-14 12:15:28 +0900

    Update plugins/starship/starship.plugin.sh

--- plugins/starship/starship.plugin.sh
+++ plugins/starship/starship.plugin.sh
@@ -3,7 +3,7 @@
 # Check if starship is installed
 if _omb_util_command_exists starship; then
     # ignore oh-my-bash theme
-    unset OSH_THEME
+    unset -v OSH_THEME
     eval "$(starship init bash)"
 else
     echo '[oh-my-bash] starship not found, please install it from https://github.com/starship/starship'
