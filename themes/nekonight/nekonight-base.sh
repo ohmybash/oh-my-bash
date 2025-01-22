548da10e5f607f90c0d612e7fe40b8f4829a07da
BrunoCiccarino <landneco0@gmail.com>
2025-01-22 00:35:22 -0300

    fix(themes): removing blank lines

--- themes/nekonight/nekonight-base.sh
+++ themes/nekonight/nekonight-base.sh
@@ -1,4 +1,3 @@
-# Função para obter informações do Git (branch e status)
 function _omb_theme_nekonight_git_prompt_info() {
   local branch_name
   branch_name=$(git symbolic-ref --short HEAD 2>&-)
@@ -11,30 +10,24 @@ function _omb_theme_nekonight_git_prompt_info() {
   echo -n "$git_status"
 }
 
-# Função para obter o status do Git
 function _omb_theme_nekonight_scm_git_status() {
   local git_status=""
 
-  # Alterações em comparação ao branch upstream
   if git rev-list --count --left-right @{upstream}...HEAD 2>&- | grep -Eq '^[0-9]+\s[0-9]+$'; then
     git_status+="${_omb_prompt_brown}↓${_omb_prompt_normal} "
   fi
 
-  # Alterações staged
   if [[ -n $(git diff --cached --name-status 2>&-) ]]; then
     git_status+="${_omb_prompt_green}+${_omb_prompt_normal}"
   fi
 
-  # Alterações unstaged
   if [[ -n $(git diff --name-status 2>&-) ]]; then
     git_status+="${_omb_prompt_yellow}•${_omb_prompt_normal}"
   fi
 
-  # Arquivos não rastreados
   if [[ -n $(git ls-files --others --exclude-standard 2>&-) ]]; then
     git_status+="${_omb_prompt_red}⌀${_omb_prompt_normal}"
   fi
 
   echo -n "$git_status"
 }
-
