217b535fbb5021c9cffbde9f07e138aeb69cbd8c
Bruno Ciccarino λ <82242552+BrunoCiccarino@users.noreply.github.com>
2025-01-22 05:25:12 -0300

    fix(themes): fix regext
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

--- themes/nekonight/nekonight-base.sh
+++ themes/nekonight/nekonight-base.sh
@@ -15,7 +15,7 @@ function _omb_theme_nekonight_git_prompt_info() {
 function _omb_theme_nekonight_scm_git_status() {
   local git_status=""
 
-  if git rev-list --count --left-right @{upstream}...HEAD 2>&- | grep -Eq '^[0-9]+\s[0-9]+$'; then
+  if git rev-list --count --left-right @{upstream}...HEAD 2>&- | grep -Eq '^[0-9]+[[:blank:]][0-9]+$'; then
     git_status+="${_omb_prompt_brown}↓${_omb_prompt_normal} "
   fi
 
