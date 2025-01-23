133a12f3b1e504455b05f917e84690203fa2da5c
BrunoCiccarino <landneco0@gmail.com>
2025-01-23 15:35:13 -0300

    refactor(): renaming all instances of git to _omb_prompt_git

--- themes/nekonight/nekonight.base.sh
+++ themes/nekonight/nekonight.base.sh
@@ -23,19 +23,19 @@ function _omb_theme_nekonight_git_prompt_info() {
 function _omb_theme_nekonight_scm_git_status() {
   local git_status=""
 
-  if git rev-list --count --left-right @{upstream}...HEAD 2>&- | grep -Eq '^[0-9]+[[:blank:]][0-9]+$'; then
+  if _omb_prompt_git rev-list --count --left-right @{upstream}...HEAD 2>&- | grep -Eq '^[0-9]+[[:blank:]][0-9]+$'; then
     git_status+="${_omb_prompt_brown}↓${_omb_prompt_normal} "
   fi
 
-  if [[ -n $(git diff --cached --name-status 2>&-) ]]; then
+  if [[ -n $(_omb_prompt_git diff --cached --name-status 2>&-) ]]; then
     git_status+="${_omb_prompt_green}+${_omb_prompt_normal}"
   fi
 
-  if [[ -n $(git diff --name-status 2>&-) ]]; then
+  if [[ -n $(_omb_prompt_git diff --name-status 2>&-) ]]; then
     git_status+="${_omb_prompt_yellow}•${_omb_prompt_normal}"
   fi
 
-  if [[ -n $(git ls-files --others --exclude-standard 2>&-) ]]; then
+  if [[ -n $(_omb_prompt_git ls-files --others --exclude-standard 2>&-) ]]; then
     git_status+="${_omb_prompt_red}⌀${_omb_prompt_normal}"
   fi
 
