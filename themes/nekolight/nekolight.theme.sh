b8665ba0760ab5c5a2a20cf89e25f4cc0a7d1e79
BrunoCiccarino <landneco0@gmail.com>
2025-01-26 04:20:15 -0300

    fix: fixxing prefixed variable name

--- themes/nekolight/nekolight.theme.sh
+++ themes/nekolight/nekolight.theme.sh
@@ -1,18 +1,25 @@
 #! bash oh-my-bash.module
 
-_omb_theme_nekolight_version='1.0.0'
+#! bash oh-my-bash.module
 
+_omb_theme_nekolight_version='1.0.0'
 _omb_theme_nekolight_symbol=""
 
 function _omb_theme_nekolight_git_info() {
   if _omb_prompt_git rev-parse --is-inside-work-tree &>/dev/null; then
-    local branch=$(_omb_prompt_git branch --show-current 2>/dev/null)
+    local branch=$(_omb_prompt_git symbolic-ref --short HEAD 2>/dev/null || _omb_prompt_git rev-parse --short HEAD 2>/dev/null)
+
+    if [[ -z $branch ]]; then
+      branch="(detached)"
+    fi
 
     if _omb_prompt_git diff --quiet 2>/dev/null && _omb_prompt_git diff --cached --quiet 2>/dev/null; then
-      _omb_util_print "on ${_omb_prompt_bold_green}${_omb_prompt_nekolight_branch} ${_omb_theme_nekolight_symbol}${_omb_prompt_normal}"
+      echo "${_omb_prompt_bold_green}${branch} ${_omb_theme_nekolight_symbol}${_omb_prompt_normal}"
     else
-      _omb_util_print "on ${_omb_prompt_bold_red}${_omb_prompt_nekolight_branch} ${_omb_theme_nekolight_symbol}${_omb_prompt_normal}"
+      echo "${_omb_prompt_bold_red}${branch} ${_omb_theme_nekolight_symbol}${_omb_prompt_normal}"
     fi
+  else
+    echo ""
   fi
 }
 
@@ -20,8 +27,8 @@ function _omb_theme_PROMPT_COMMAND() {
   local display_dir="${_omb_prompt_bold_blue}\w${_omb_prompt_normal}"
   local git_status=$(_omb_theme_nekolight_git_info)
 
-  PS1="${_omb_prompt_nekolight_dir} ${_omb_prompt_nekolight_git_status}\n❯ "
-
+  PS1="${display_dir} ${git_status}\n❯ "
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
+
