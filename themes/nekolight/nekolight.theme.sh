4a47c4ea4b974dc1065e0fa7672075066604d58d
BrunoCiccarino <landneco0@gmail.com>
2025-01-26 03:41:24 -0300

    fix: fixxing identation and  nomeclature

--- themes/nekolight/nekolight.theme.sh
+++ themes/nekolight/nekolight.theme.sh
@@ -2,25 +2,25 @@
 
 export _omb_prompt_nekolight_version='1.0.0'
 
-symbol=""
+_omb_theme_nekolight_symbol=""
 
 function _omb_theme_nekolight_git_info() {
-    if _omb_prompt_git rev-parse --is-inside-work-tree &>/dev/null; then
-        local _omb_prompt_nekolight_branch=$(_omb_prompt_git branch --show-current 2>/dev/null)
+  if _omb_prompt_git rev-parse --is-inside-work-tree &>/dev/null; then
+    local _omb_prompt_nekolight_branch=$(_omb_prompt_git branch --show-current 2>/dev/null)
 
-        if _omb_prompt_git diff --quiet 2>/dev/null && _omb_prompt_git diff --cached --quiet 2>/dev/null; then
-            echo -e "${_omb_prompt_bold_green}${_omb_prompt_nekolight_branch} ${symbol}${_omb_prompt_normal}"  # Green for clean
-        else
-            echo -e "${_omb_prompt_bold_red}${_omb_prompt_nekolight_branch} ${symbol}${_omb_prompt_normal}"  # Red for dirty
-        fi
+    if _omb_prompt_git diff --quiet 2>/dev/null && _omb_prompt_git diff --cached --quiet 2>/dev/null; then
+      _omb_util_print "${_omb_prompt_bold_green}${_omb_prompt_nekolight_branch} ${_omb_theme_nekolight_symbol}${_omb_prompt_normal}"
+    else
+      _omb_util_print "${_omb_prompt_bold_red}${_omb_prompt_nekolight_branch} ${_omb_theme_nekolight_symbol}${_omb_prompt_normal}"
+      fi
     fi
 }
 
 function _omb_theme_PROMPT_COMMAND() {
-    local _omb_prompt_nekolight_dir="${_omb_prompt_bold_blue}\w${_omb_prompt_normal}"
-    local _omb_prompt_nekolight_git_status=$(_omb_theme_nekolight_git_info)
+  local _omb_prompt_nekolight_dir="${_omb_prompt_bold_blue}\w${_omb_prompt_normal}"
+  local _omb_prompt_nekolight_git_status=$(_omb_theme_nekolight_git_info)
 
-    PS1="${_omb_prompt_nekolight_dir} ${_omb_prompt_nekolight_git_status}\n❯ "
+  PS1="${_omb_prompt_nekolight_dir} ${_omb_prompt_nekolight_git_status}\n❯ "
 
 }
 
