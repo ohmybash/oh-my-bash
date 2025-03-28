18594e4a4330a6440c075ed9ea5b5c76caf8ece9
sjplanet <162552669+sjplanet@users.noreply.github.com>
2025-03-28 13:14:45 +0100

    more checking of CARGO_HOME and PATH

--- plugins/cargo/cargo.plugin.sh
+++ plugins/cargo/cargo.plugin.sh
@@ -1,9 +1,14 @@
 #! bash oh-my-bash.module
 # @sjplanet 2025
 
-if [ -d ~/.cargo ]; then
-	# cargo exported variables
-	export CARGO_HOME=~/.cargo
-	export PATH=$CARGO_HOME/bin${PATH:+:$PATH}
+# Set CARGO_HOME to default if not already set
+: "${CARGO_HOME:=$HOME/.cargo}"
 
+# Check if the directory exists
+if [ -d "$CARGO_HOME" ]; then
+    # Add $CARGO_HOME/bin to PATH if it's not already included
+    case ":$PATH:" in
+        *":$CARGO_HOME/bin:"*) ;;  # already in PATH, do nothing
+        *) export PATH="$CARGO_HOME/bin${PATH:+:$PATH}" ;;
+    esac
 fi
