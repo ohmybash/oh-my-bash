30f026d40aafbeb9521575d6ed8b708b36513d57
Koichi Murase <myoga.murase@gmail.com>
2025-04-20 02:23:05 +0900

    fix(custom/aliases/example): !fixup

--- custom/aliases/example.aliases.sh
+++ custom/aliases/example.aliases.sh
@@ -1,3 +1,4 @@
 # Add your own custom alias in the custom/aliases directory. Aliases placed
 # here will override ones with the same name in the main alias directory.
-# Remember to include your custom alias files in the alias array in ~/.bashrc
+# Remember to include your custom alias files in the "aliases" array in
+# ~/.bashrc
