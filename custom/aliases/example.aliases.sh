a14057d131557543740a556ccc412b2648aea039
gunnar <vrms@netcologne.de>
2025-04-19 20:58:25 +0200

    docs(custom/aliases/example): explain how to add and use custom alias (#499)
    This patch adds an explanation on how to use
    './custom/aliases/example.aliases.sh'
    
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>
    Co-authored-by: Rob Loach <RobLoach@users.noreply.github.com>

--- custom/aliases/example.aliases.sh
+++ custom/aliases/example.aliases.sh
@@ -1,2 +1,10 @@
 # Add your own custom alias in the custom/aliases directory. Aliases placed
 # here will override ones with the same name in the main alias directory.
+#
+# Usage:
+#
+# 1. use the exact naming schema like '<my_aliases>.aliases.sh' where the
+#    filename needs to end with .aliases.sh (just <my_aliases>.sh does not
+#    work)
+# 2. add the leading part of that filename ('<my_aliases>' in this example) to
+#    the 'aliases' array in your ~/.bashrc
