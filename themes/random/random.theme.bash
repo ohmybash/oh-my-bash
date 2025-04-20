1631640ace2449057b83d8e4ad6d46b0b7821518
Koichi Murase <myoga.murase@gmail.com>
2025-04-20 12:04:28 +0900

    refactor(themes/random): add function "_omb_theme_random_list_available_themes"

--- themes/random/random.theme.bash
+++ themes/random/random.theme.bash
@@ -2,18 +2,21 @@
 
 # This theme loads another randomly-chosen theme.
 
-_omb_util_glob_expand _omb_init_files '"$OSH"/themes/*/*.theme.sh'
+## @var[out] _omb_init_files
+function _omb_theme_random__list_available_themes {
+  _omb_util_glob_expand _omb_init_files '"$OSH"/themes/*/*.theme.sh'
 
-# Remove ignored themes from the list
-for _omb_init_theme in random "${OMB_THEME_RANDOM_IGNORED[@]}"; do
-  for _omb_init_index in "${!_omb_init_files[@]}"; do
-    [[ ${_omb_init_files[_omb_init_index]} == */"$_omb_init_theme"/* ]] &&
-      unset -v '_omb_init_files[_omb_init_index]'
+  # Remove ignored themes from the list
+  local theme index
+  for theme in random "${OMB_THEME_RANDOM_IGNORED[@]}"; do
+    for index in "${!_omb_init_files[@]}"; do
+      [[ ${_omb_init_files[index]} == */"$theme"/* ]] &&
+        unset -v '_omb_init_files[index]'
+    done
   done
-  unset -v _omb_init_index
-done
-unset -v _omb_init_theme
-_omb_init_files=("${_omb_init_files[@]}")
+  _omb_init_files=("${_omb_init_files[@]}")
+}
+_omb_theme_random__list_available_themes
 
 if ((${#_omb_init_files[@]})); then
   _omb_init_file=${_omb_init_files[RANDOM%${#_omb_init_files[@]}]}
