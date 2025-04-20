dbd75157ec813b8b8281282a619e3b366074aae8
Koichi Murase <myoga.murase@gmail.com>
2025-04-20 12:18:34 +0900

    fix(themes/random): check the theme filename
    We have been allowing the filename of the form
    "themes/<A>/<B>.theme.sh", but we should require that <A> matches <B>.

--- themes/random/random.theme.bash
+++ themes/random/random.theme.bash
@@ -2,28 +2,41 @@
 
 # This theme loads another randomly-chosen theme.
 
-## @var[out] _omb_init_files
+## @var[out] _omb_init_themes
 function _omb_theme_random__list_available_themes {
-  _omb_util_glob_expand _omb_init_files '"$OSH"/themes/*/*.theme.sh'
+  _omb_init_themes=()
+
+  local theme_files
+  _omb_util_glob_expand theme_files '"$OSH"/themes/*/*.theme.sh'
+  ((${#theme_files[@]})) || return 1
+
+  local -a themes=() index theme
+  for index in "${!theme_files[@]}"; do
+    theme=${theme_files[index]##*/}
+    theme=${theme%.theme.sh}
+    [[ ${theme_files[index]} == */"$theme"/"$theme".theme.sh ]] || continue
+
+    _omb_util_array_contains themes "$theme" ||
+      themes+=("$theme")
+  done
+  ((${#themes[@]})) || return 1
 
   # Remove ignored themes from the list
   local theme index
   for theme in random "${OMB_THEME_RANDOM_IGNORED[@]}"; do
-    for index in "${!_omb_init_files[@]}"; do
-      [[ ${_omb_init_files[index]} == */"$theme"/* ]] &&
-        unset -v '_omb_init_files[index]'
+    for index in "${!themes[@]}"; do
+      [[ ${themes[index]} == "$theme" ]] &&
+        unset -v 'themes[index]'
     done
   done
-  _omb_init_files=("${_omb_init_files[@]}")
+  ((${#themes[@]})) || return 1
+  _omb_init_themes=("${themes[@]}")
 }
 _omb_theme_random__list_available_themes
 
-if ((${#_omb_init_files[@]})); then
-  _omb_init_file=${_omb_init_files[RANDOM%${#_omb_init_files[@]}]}
-  source "$_omb_init_file"
-  OMB_THEME_RANDOM_SELECTED=${_omb_init_file##*/}
-  OMB_THEME_RANDOM_SELECTED=${OMB_THEME_RANDOM_SELECTED%.theme.bash}
-  OMB_THEME_RANDOM_SELECTED=${OMB_THEME_RANDOM_SELECTED%.theme.sh}
-  printf '%s\n' "[oh-my-bash] Random theme '$OMB_THEME_RANDOM_SELECTED' ($_omb_init_file) loaded..."
+if ((${#_omb_init_themes[@]})); then
+  OMB_THEME_RANDOM_SELECTED=${_omb_init_themes[RANDOM%${#_omb_init_themes[@]}]}
+  _omb_module_require_theme "$OMB_THEME_RANDOM_SELECTED"
+  printf '%s\n' "[oh-my-bash] Random theme '$OMB_THEME_RANDOM_SELECTED' loaded..."
 fi
-unset -v _omb_init_files _omb_init_file
+unset -v _omb_init_themes
