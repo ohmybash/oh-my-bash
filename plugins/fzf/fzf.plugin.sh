5a1086cfd423c1331338e6f3333dbe44e421b936
Martin Atukunda <matlads@gmail.com>
2025-04-27 16:05:07 +0300

    fix: use recommended way to load fzf into bash
    use `eval "$(fzf --bash)"` instead of checking for the existance of the
    `~/.fzf/fzf.bash` file. This way we can support all the various ways of
    installing fzf onto the users system.

--- plugins/fzf/fzf.plugin.sh
+++ plugins/fzf/fzf.plugin.sh
@@ -2,7 +2,8 @@
 
 # Check if fzf is installed
 if _omb_util_command_exists fzf; then
-    [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+    # Set up fzf key bindings and fuzzy completion
+    eval "$(fzf --bash)"
 else
     _omb_util_print '[oh-my-bash] fzf not found, please install it from https://github.com/junegunn/fzf' >&2
 fi
