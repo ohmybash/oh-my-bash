c6da5782d991be0ef3181b52fb733c3d1995ea47
Koichi Murase <myoga.murase@gmail.com>
2025-05-06 16:46:47 +0900

    perf(completions/kubectl): disable slow kubectl plugins in generating completion setting

--- completions/kubectl.completion.sh
+++ completions/kubectl.completion.sh
@@ -4,5 +4,12 @@
 
 if _omb_util_command_exists kubectl
 then
-  eval "$(kubectl completion bash)"
+  # Note: We set KUBECONFIG=/dev/null to disable any kubectl plugins that take
+  # time to initialize. For example, gke-gcloud-auth-plugin provided by Google
+  # Cloud for GKE (Google Kubernetes Engine) seems to try to connect to an auth
+  # server every time the kubectl command is executed, regardless of whether
+  # the kubectl subcommand requires authentication. Since the generation of the
+  # completion script should not depend on plugins, we temporarily disable all
+  # plugins by setting a temporary environment KUBECONFIG=/dev/null.
+  eval "$(KUBECONFIG=/dev/null kubectl completion bash)"
 fi
