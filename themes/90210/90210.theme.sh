9effeebc5222e1757e04052c985884bbf4710f83
Wilton Rodrigues <wiltonsr94@gmail.com>
2025-05-08 18:05:04 -0300

    feat: add support to python venv in 90210 theme

--- themes/90210/90210.theme.sh
+++ themes/90210/90210.theme.sh
@@ -11,9 +11,12 @@ GIT_THEME_PROMPT_SUFFIX="${_omb_prompt_green}|"
 
 # Nicely formatted terminal prompt
 function _omb_theme_PROMPT_COMMAND(){
+  local python_venv
+  _omb_prompt_get_python_venv
+
   PS1="\n${_omb_prompt_bold_gray}[${_omb_prompt_navy}\@${_omb_prompt_bold_gray}]-"
   PS1+="${_omb_prompt_bold_gray}[${_omb_prompt_green}\u${_omb_prompt_olive}@${_omb_prompt_green}\h${_omb_prompt_bold_gray}]-"
-  PS1+="${_omb_prompt_bold_gray}[${_omb_prompt_purple}\w${_omb_prompt_bold_gray}]-"
+  PS1+="${_omb_prompt_bold_gray}[${_omb_prompt_purple}\w${_omb_prompt_bold_gray}]-${python_venv}"
   PS1+="$(scm_prompt_info)\n${_omb_prompt_reset_color}\$ "
 }
 
