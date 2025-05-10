77d9723c94ff42f079be2a83b717df63ccd477a7
NN <580536+NN---@users.noreply.github.com>
2025-05-10 20:32:06 +0300

    Add zellij autoattach plugin

---
+++ plugins/zellij-autoattach/zellij-autoattach.plugin.sh
@@ -0,0 +1,25 @@
+#! bash oh-my-bash.module
+# @NN--- 2025
+#
+# zellij-autoattach.plugin.sh
+#
+# A zellij plugin that will automatically attach itself to a bash session.
+
+[[ $OSH_PLUGIN_ZELLIJ_AUTOATTACH_BEHAVIOR ]] || OSH_PLUGIN_ZELLIJ_AUTOATTACH_BEHAVIOR="detach"
+
+function _osh_plugin_zellij_autoattach_exit {
+  [[ -z $ZELLIJ ]] && zellij attach --create default && exit
+}
+
+function _osh_plugin_zellij_autoattach_detach {
+  [[ -z $ZELLIJ ]] && zellij attach --create default
+}
+
+case $OSH_PLUGIN_ZELLIJ_AUTOATTACH_BEHAVIOR in
+'exit')
+  _osh_plugin_zellij_autoattach_exit
+  ;;
+'detach' | *)
+  _osh_plugin_zellij_autoattach_detach
+  ;;
+esac
