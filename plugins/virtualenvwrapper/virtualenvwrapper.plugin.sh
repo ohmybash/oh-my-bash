8f56c93f3a9c6f404e3d767cfcf01e88c851d1f2
Koichi Murase <myoga.murase@gmail.com>
2025-06-14 22:43:27 +0900

    Apply suggestions from code review

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.sh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.sh
@@ -2,8 +2,8 @@
 
 # Check if virtualenvwrapper is installed
 if _omb_util_command_exists virtualenvwrapper.sh; then
-    # Source the virtualenvwrapper script
-    source virtualenvwrapper.sh
+  # Source the virtualenvwrapper script
+  source virtualenvwrapper.sh
 else
-    _omb_util_print '[oh-my-bash] virtualenvwrapper not found, please install it from https://github.com/python-virtualenvwrapper/virtualenvwrapper' >&2
+  _omb_util_print '[oh-my-bash] virtualenvwrapper not found, please install it from https://github.com/python-virtualenvwrapper/virtualenvwrapper' >&2
 fi
