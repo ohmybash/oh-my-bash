20fc32f69bf0ca095ede3769366b7f7e5c711989
Martin Atukunda <matlads@gmail.com>
2025-06-14 14:33:17 +0300

    remove use of `which` as it is not POSIX.
    > which is not POSIX. Actually, source by default searches PATH for the
    > file to read
    spot on.

--- plugins/virtualenvwrapper/virtualenvwrapper.plugin.sh
+++ plugins/virtualenvwrapper/virtualenvwrapper.plugin.sh
@@ -3,7 +3,7 @@
 # Check if virtualenvwrapper is installed
 if _omb_util_command_exists virtualenvwrapper.sh; then
     # Source the virtualenvwrapper script
-    source "$(which virtualenvwrapper.sh)"
+    source virtualenvwrapper.sh
 else
     _omb_util_print '[oh-my-bash] virtualenvwrapper not found, please install it from https://github.com/python-virtualenvwrapper/virtualenvwrapper' >&2
 fi
