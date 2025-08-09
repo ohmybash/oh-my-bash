f84fff4536f83131017108b94b63210cf3cb9a3d
Martin Atukunda <matlads@users.noreply.github.com>
2025-08-10 01:06:17 +0300

    Update completions/jump.completion.sh
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

--- completions/jump.completion.sh
+++ completions/jump.completion.sh
@@ -4,7 +4,7 @@ if _omb_util_command_exists jump; then
 	_jump()
 	{
 	    local JUMP_COMMANDS=$(jump 2>&1 | command grep -E '^ +' | awk '{print $1}')
-	    COMPREPLY=( $(compgen -W "$JUMP_COMMANDS" -- "${COMP_WORDS[1]}") )
+	    COMPREPLY=( $(compgen -W '$JUMP_COMMANDS' -- "${COMP_WORDS[1]}") )
 	}
 	complete -F _jump jump
 fi
