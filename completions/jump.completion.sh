72c91ba5987dc76684365acbc95e789d02531546
Martin Atukunda <matlads@gmail.com>
2025-08-11 05:46:07 +0300

    fix: indentation to use two spaces

--- completions/jump.completion.sh
+++ completions/jump.completion.sh
@@ -1,10 +1,10 @@
 #! bash oh-my-bash.module
 
 if _omb_util_command_exists jump; then
-  function _omb_plugin_jump_completion {
-	{
-	    local JUMP_COMMANDS=$(jump 2>&1 | command grep -E '^ +' | awk '{print $1}')
-	    COMPREPLY=( $(compgen -W '$JUMP_COMMANDS' -- "${COMP_WORDS[1]}") )
-	}
-	complete -F _omb_plugin_jump_completion jump
+  function _omb_plugin_jump_completion
+  {
+    local JUMP_COMMANDS=$(jump 2>&1 | command grep -E '^ +' | awk '{print $1}')
+    COMPREPLY=( $(compgen -W '$JUMP_COMMANDS' -- "${COMP_WORDS[1]}") )
+  }
+  complete -F _omb_plugin_jump_completion jump
 fi
