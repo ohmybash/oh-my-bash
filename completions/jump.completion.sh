0eb38c529e52652fc95991fbd9d0f40057f8f01b
Koichi Murase <myoga.murase@gmail.com>
2025-08-12 17:19:11 +0900

    Update completions/jump.completion.sh

--- completions/jump.completion.sh
+++ completions/jump.completion.sh
@@ -1,8 +1,7 @@
 #! bash oh-my-bash.module
 
 if _omb_util_command_exists jump; then
-  function _omb_plugin_jump_completion
-  {
+  function _omb_plugin_jump_completion {
     local JUMP_COMMANDS=$(jump 2>&1 | command grep -E '^ +' | awk '{print $1}')
     COMPREPLY=( $(compgen -W '$JUMP_COMMANDS' -- "${COMP_WORDS[1]}") )
   }
