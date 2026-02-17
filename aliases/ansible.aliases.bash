d141f2289f05382a5de4b169235eb16c1cd3dd6a
Pascal Kontschan <pascal.kontschan@sva.de>
2026-02-17 18:07:27 +0100

    added ansible aliases

---
+++ aliases/ansible.aliases.bash
@@ -0,0 +1,18 @@
+#! bash oh-my-bash.module
+
+# used to be functions in previous ansible oh-my-bash plugin
+alias aver='ansible --version; ansible-community --version'
+#alias arinit='ansible-role-init'   # this alias can now be found in the ansible-dev plugin
+
+# ansible-core
+alias a='ansible '
+alias aconf='ansible-config '
+alias acon='ansible-console '
+alias adoc='ansible-doc '
+alias agal='ansible-galaxy '
+alias ainv='ansible-inventory '
+alias aplay='ansible-playbook '
+alias aplaybook='aplay' # long version kept for backward compatibility
+alias apull='ansible-pull '
+alias atest='ansible-test '
+alias aval='ansible-vault '
