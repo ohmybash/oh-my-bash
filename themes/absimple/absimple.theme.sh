fdc23047424c4fcebbc02955fa183b28c00e4214
Koichi Murase <myoga.murase@gmail.com>
2026-03-01 20:17:11 +0900

    style(themes/absimple): fix indentation

--- themes/absimple/absimple.theme.sh
+++ themes/absimple/absimple.theme.sh
@@ -26,44 +26,44 @@ source "$OSH/themes/agnoster/agnoster.theme.sh"
 # prints history followed by HH:MM, useful for remembering what
 # we did previously
 function prompt_histdt {
-    prompt_segment black default "\!" # \A"
+  prompt_segment black default "\!" # \A"
 }
 
 # Dir: current working directory
 function prompt_dir {
-    prompt_segment blue black '\W'
+  prompt_segment blue black '\W'
 }
 
 ######################################################################
 
 # quick right prompt I grabbed to test things.
 function __command_rprompt {
-    local times= n=$COLUMNS tz
-    for tz in ZRH:Europe/Zurich PIT:US/Eastern \
-              MTV:US/Pacific TOK:Asia/Tokyo; do
-        [ $n -gt 40 ] || break
-        times="$times ${tz%%:*}\e[30;1m:\e[0;36;1m"
-        times="$times$(TZ=${tz#*:} date +%H:%M:%S)\e[0m"
-        n=$(( $n - 10 ))
-    done
-    [ -z "$times" ] || printf "%${n}s$times\\r" ''
+  local times= n=$COLUMNS tz
+  for tz in ZRH:Europe/Zurich PIT:US/Eastern \
+            MTV:US/Pacific TOK:Asia/Tokyo; do
+    [ $n -gt 40 ] || break
+    times="$times ${tz%%:*}\e[30;1m:\e[0;36;1m"
+    times="$times$(TZ=${tz#*:} date +%H:%M:%S)\e[0m"
+    n=$(( $n - 10 ))
+  done
+  [ -z "$times" ] || printf "%${n}s$times\\r" ''
 }
 
 ######################################################################
 ## Main prompt
 
 function build_prompt {
-    [[ ! -z ${AG_EMACS_DIR+x} ]] && prompt_emacsdir
-    prompt_status
-    [[ -z ${AG_NO_HIST+x} ]] && prompt_histdt
-    #[[ -z ${AG_NO_CONTEXT+x} ]] && prompt_context
-    if [[ ${OMB_PROMPT_SHOW_PYTHON_VENV-} ]]; then
-      prompt_virtualenv
-      prompt_condaenv
-    fi
-    prompt_dir
-    prompt_git
-    prompt_end
+  [[ ! -z ${AG_EMACS_DIR+x} ]] && prompt_emacsdir
+  prompt_status
+  [[ -z ${AG_NO_HIST+x} ]] && prompt_histdt
+  #[[ -z ${AG_NO_CONTEXT+x} ]] && prompt_context
+  if [[ ${OMB_PROMPT_SHOW_PYTHON_VENV-} ]]; then
+    prompt_virtualenv
+    prompt_condaenv
+  fi
+  prompt_dir
+  prompt_git
+  prompt_end
 }
 
 # from orig...
@@ -72,23 +72,23 @@ function build_prompt {
 # use that.
 
 function _omb_theme_PROMPT_COMMAND {
-    local RETVAL=$?
-    local PRIGHT=""
-    local CURRENT_BG=NONE
-    local PR="$(ansi_single $(text_effect reset))"
-    build_prompt
+  local RETVAL=$?
+  local PRIGHT=""
+  local CURRENT_BG=NONE
+  local PR="$(ansi_single $(text_effect reset))"
+  build_prompt
 
-    PS1=""
-    # date randomly or once per hour
-    if (( $(shuf -i 1-20 -n 1 --random-source=/dev/urandom) == 1 )) ; then #TK || (($PSDATE != $(date +%H))) ; then
-        PS1+="\$(date +%a) $(date +%Y-%m-%d) "
-    fi
-    PSDATE=$(date +%H)
-    # ... and time
-    PS1+="$(date +%H:%M:%S) "
+  PS1=""
+  # date randomly or once per hour
+  if (( $(shuf -i 1-20 -n 1 --random-source=/dev/urandom) == 1 )) ; then #TK || (($PSDATE != $(date +%H))) ; then
+    PS1+="\$(date +%a) $(date +%Y-%m-%d) "
+  fi
+  PSDATE=$(date +%H)
+  # ... and time
+  PS1+="$(date +%H:%M:%S) "
 
-    # uncomment below to use right prompt
-    #     PS1='\[$(tput sc; printf "%*s" $COLUMNS "$PRIGHT"; tput rc)\]'$PR
-    PS1+=$PR
+  # uncomment below to use right prompt
+  #     PS1='\[$(tput sc; printf "%*s" $COLUMNS "$PRIGHT"; tput rc)\]'$PR
+  PS1+=$PR
 }
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
